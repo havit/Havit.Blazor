e6b94fdfd287ba70f3629c38240447aca7c99e7c
Dominik Crha <crha@havit.cz>
2020-05-14 15:26:07 +0200

    NamedViewList show/hide button

--- Havit.Blazor.Components.Web.Bootstrap/HxListLayout.razor.cs
+++ Havit.Blazor.Components.Web.Bootstrap/HxListLayout.razor.cs
@@ -9,39 +9,47 @@ namespace Havit.Blazor.Components.Web.Bootstrap
 {
     public partial class HxListLayout
     {
-		[Parameter]
-		public bool FilterExpanded { get; set; }
+        [Parameter]
+        public bool FilterExpanded { get; set; }
 
-		[Parameter]
-		public string Title { get; set; }
+        [Parameter]
+        public string Title { get; set; }
 
-		[Parameter]
-		// TODO: TitleTemplate nebo TitleSection? Bude mít vůbec význam?
-		public RenderFragment TitleSection { get; set; }
+        [Parameter]
+        // TODO: TitleTemplate nebo TitleSection? Bude mít vůbec význam?
+        public RenderFragment TitleSection { get; set; }
 
-		[Parameter]
-		// TODO: QuickSearchSection?
-		public RenderFragment SearchSection { get; set; }
+        [Parameter]
+        // TODO: QuickSearchSection?
+        public RenderFragment SearchSection { get; set; }
 
-		[Parameter]
-		// TODO: Search (předchozí) vs. Filter (zde)
-		public RenderFragment FilterSection { get; set; }
+        [Parameter]
+        // TODO: Search (předchozí) vs. Filter (zde)
+        public RenderFragment FilterSection { get; set; }
 
-		[Parameter]
-		public RenderFragment NamedViewsSection { get; set; }
+        [Parameter]
+        public RenderFragment NamedViewsSection { get; set; }
+        [Parameter]
+        public bool NamedViewsSectionExpanded { get; set; }
+        private string NamedViewsSectionExpandedClass => NamedViewsSectionExpanded ? "show" : null;
 
-		[Parameter]
-		public RenderFragment DataSection { get; set; }
+        [Parameter]
+        public RenderFragment DataSection { get; set; }
 
-		[Parameter]
-		public RenderFragment DetailSection { get; set; }
+        [Parameter]
+        public RenderFragment DetailSection { get; set; }
 
-		[Parameter]
-		public RenderFragment CommandsSection { get; set; }
+        [Parameter]
+        public RenderFragment CommandsSection { get; set; }
 
-		private void ToggleFilterExpanded()
+        private void ToggleFilterExpanded()
         {
-			FilterExpanded = !FilterExpanded;
+            FilterExpanded = !FilterExpanded;
         }
-	}
+
+        private void ToggleNamedViewsSectionExpanded()
+        {
+            NamedViewsSectionExpanded = !NamedViewsSectionExpanded;
+        }
+    }
 }
