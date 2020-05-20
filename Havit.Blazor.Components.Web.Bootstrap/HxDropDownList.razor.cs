f4ccab2605377044abdc6fe3dae347410b0845fb
Jiří Kanda <kanda@havit.cz>
2020-05-20 21:14:52 +0200

    Zapojení HxButton

---
+++ Havit.Blazor.Components.Web.Bootstrap/HxDropDownList.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit.GoranG3.Web.Client.Pages.Prototyping
+{
+    public class HxDropDownList<T> : ComponentBase
+    {
+        [Parameter]
+		public T Value { get; set; }
+
+		[Parameter]
+		public EventCallback<EventArgs> OnValueChanged { get; set; }
+
+		[Parameter]
+		public IEnumerable<T> Data { get; set; }
+	}
+}
