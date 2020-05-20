124708e4f11a0f0c3a24d90e6cbf55b5a433e8a2
Jiří Kanda <kanda@havit.cz>
2020-05-20 20:51:45 +0200

    HxButton

---
+++ Havit.Blazor.Components.Web.Bootstrap/HxButton.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit.Blazor.Components.Web.Bootstrap
+{
+	public partial class HxButton : ComponentBase
+	{
+		[Parameter]
+		public string Text { get; set; }
+
+		[Parameter]
+		public RenderFragment ChildContent { get; set; }
+
+		[Parameter]
+		public string Skin { get; set; } // TODO
+
+		[Parameter]
+		public bool Enabled { get; set; } // TODO
+
+		[Parameter]
+		public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+		private async Task ButtonClicked(MouseEventArgs mouseEventArgs)
+		{
+			await OnClick.InvokeAsync(mouseEventArgs);
+		}
+	}
+}
