7e6c5e45a627cb80a131de64e982f767134f7b00
Dominik Crha <crha@havit.cz>
2020-05-21 16:24:20 +0200

    HxTag, HxTagInput

---
+++ Havit.Blazor.Components.Web.Bootstrap/HxTag.razor.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit.Blazor.Components.Web.Bootstrap
+{
+    public partial class HxTag
+    {
+        [Parameter]
+        public string Text { get; set; }
+    }
+}
