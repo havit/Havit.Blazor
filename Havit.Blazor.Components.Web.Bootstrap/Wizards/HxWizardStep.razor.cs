347cf1eb275dabb30cb1ac513d70b6651723a3b4
Jiří Kanda <kanda@havit.cz>
2020-05-26 08:55:01 +0200

    HxWizard (základní nahození)

---
+++ Havit.Blazor.Components.Web.Bootstrap/Wizards/HxWizardStep.razor.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
+
+namespace Havit.Blazor.Components.Web.Bootstrap.Wizards
+{
+	/// <summary>
+	/// Wizard Step component
+	/// </summary>
+	public partial class HxWizardStep : ComponentBase, IDisposable, IHxWizardStep
+	{
+		/*/// <summary>
+		/// The Name of the step
+		///</summary>
+		[Parameter]
+		public string Name { get; set; }*/
+
+		/// <summary>
+		/// The Child Content of the current <see cref="WizardStep" />
+		/// </summary>
+		[Parameter]
+		public RenderFragment Content { get; set; }
+
+		[CascadingParameter(Name = "WizardStepsRegistration")] // TODO Konstanta
+		public CollectionRegistration<IHxWizardStep> StepsRegistration { get; set; }
+
+		[Parameter]
+		public bool IsActive { get; set; } // TODO: nebo activestep? Nedělal bych obojí. Ale jak zvenku nastavit aktuální krok?
+
+		[Parameter]
+		public EventCallback<bool> IsActiveChanged { get; set; }
+
+		public async Task Activate()
+		{
+			if (!IsActive)
+			{
+				IsActive = true;
+				await IsActiveChanged.InvokeAsync(true);
+			}
+		}
+
+		public async Task Deactivate()
+		{
+			if (IsActive)
+			{
+				IsActive = false;
+				await IsActiveChanged.InvokeAsync(false);
+			}
+		}
+
+		protected override void OnInitialized()
+		{
+			StepsRegistration.Register(this);
+		}
+
+		public void Dispose()
+		{
+			StepsRegistration.Unregister(this);
+		}
+	}
+}
---
+++ Havit.Blazor.Components.Web.Bootstrap/Wizards/IHxWizardStep.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit.Blazor.Components.Web.Bootstrap.Wizards
+{
+	public interface IHxWizardStep
+	{
+		bool IsActive { get; }
+		Task Activate();
+		Task Deactivate();
+	}
+}
