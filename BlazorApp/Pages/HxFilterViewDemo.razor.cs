e443a14be78916305c2e83b3d894c2b9e6245b53
Jiří Kanda <kanda@havit.cz>
2020-05-27 12:04:27 +0200

    Debugging app - filtrers

---
+++ BlazorApp/Pages/HxFilterViewDemo.razor.cs
@@ -0,0 +1,19 @@
+﻿using Havit.Blazor.Components.Web.Bootstrap.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Pages
+{
+	public partial class HxFilterViewDemo
+	{
+		public CustomFilter Filter { get; } = new CustomFilter();
+
+		public class CustomFilter
+		{
+			public DateRange MyDateRange { get; set; }
+			public string MyText { get; set; }
+		}
+	}
+}
