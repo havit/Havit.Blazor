0a3250e62aaa994dce5039077173514c267ab131
Kanda, Jiří <kanda@havit.cz>
2020-06-04 16:12:18 +0200

    Testovací scénář pro aktualizaci filtru.

--- BlazorApp/Pages/HxFilterViewDemo.razor.cs
+++ BlazorApp/Pages/HxFilterViewDemo.razor.cs
@@ -8,12 +8,17 @@ namespace BlazorApp.Pages
 {
 	public partial class HxFilterViewDemo
 	{
-		public CustomFilter Filter { get; } = new CustomFilter();
+		public CustomFilter Filter { get; set; } = new CustomFilter();
 
 		public class CustomFilter
 		{
 			public DateRange MyDateRange { get; set; }
 			public string MyText { get; set; }
+		}
+
+		private void SetRandomFilter()
+		{
+			Filter = new CustomFilter() { MyText = Guid.NewGuid().ToString() };
 		}
 	}
 }
