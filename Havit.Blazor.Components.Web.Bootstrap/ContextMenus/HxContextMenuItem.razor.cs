7432c179dcaab113896df40037daae69677f1b6a
Jiří Kanda <kanda@havit.cz>
2020-06-19 16:26:28 +0200

    HxContextMenuItem - podpora pro ConfirmationQuestion(zatím pomocí js confirm).

--- Havit.Blazor.Components.Web.Bootstrap/ContextMenus/HxContextMenuItem.razor.cs
+++ Havit.Blazor.Components.Web.Bootstrap/ContextMenus/HxContextMenuItem.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -15,10 +16,23 @@ namespace Havit.Blazor.Components.Web.Bootstrap.ContextMenus
 		public string Title { get; set; }
 
 		[Parameter]
+		public string ConfirmationQuestion { get; set; }
+
+		[Parameter]
 		public EventCallback OnItemClick { get; set; }
 
-		public async Task OnClick()
+		[Inject]
+		public IJSRuntime JSRuntime { get; set; }
+		
+		public async Task HandleClick()
 		{
+			if (!String.IsNullOrEmpty(ConfirmationQuestion))
+			{
+				if (!await JSRuntime.InvokeAsync<bool>("confirm", ConfirmationQuestion))
+				{
+					return; // No Action
+				}
+			}
 			await OnItemClick.InvokeAsync(null);
 		}
 	}
