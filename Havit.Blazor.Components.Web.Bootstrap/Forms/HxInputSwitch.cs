e47aef53913fd51a530e0c18d460b7877eb5aa6d
Kanda, Jiří <kanda@havit.cz>
2020-06-22 15:43:03 +0200

    HxInputSwitch

---
+++ Havit.Blazor.Components.Web.Bootstrap/Forms/HxInputSwitch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Havit.Blazor.Components.Web.Bootstrap.Forms
+{
+	/// <summary>
+	/// Switch input.
+	/// </summary>
+	public class HxInputSwitch : HxInputCheckBox
+	{
+		/// <inheritdoc />
+		private protected override string CoreCssClass => "form-check form-switch";
+	}
+}
