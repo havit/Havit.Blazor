b8a4cdd92669eb62cfe5f256171fbbd6048c61e0
Viliam Kortiš <kortis@havit.cz>
2020-07-21 13:51:12 +0200

    50618 Blazor: Připravit vizuální podobu pro skiny tlačítek - ikony
    Related Work Items: #50618

--- Havit.Blazor.Components.Web.Bootstrap/Buttons/ButtonSkins.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Buttons/ButtonSkins.cs
@@ -14,18 +14,18 @@ namespace Havit.Blazor.Components.Web.Bootstrap.Buttons
 		
 		// DO NOT FORGET TO MAINTAIN RESOURCES!
 
-		public static ButtonSkin Apply { get; } = new ButtonSkin(text: "Apply", resourceType: typeof(ButtonSkins));
-		public static ButtonSkin Cancel { get; } = new ButtonSkin(text: "Cancel", resourceType: typeof(ButtonSkins));
-		public static ButtonSkin Close { get; } = new ButtonSkin(text: "Close", resourceType: typeof(ButtonSkins));
-		public static ButtonSkin Delete { get; } = new ButtonSkin(text: "Delete", resourceType: typeof(ButtonSkins));
-		public static ButtonSkin Edit { get; } = new ButtonSkin(text: "Edit", resourceType: typeof(ButtonSkins));
-		public static ButtonSkin Export { get; } = new ButtonSkin(text: "Export", resourceType: typeof(ButtonSkins));
-		public static ButtonSkin Insert { get; } = new ButtonSkin(text: "Insert", resourceType: typeof(ButtonSkins));
-		public static ButtonSkin New { get; } = new ButtonSkin(text: "New", resourceType: typeof(ButtonSkins));
-		public static ButtonSkin OK { get; } = new ButtonSkin(text: "OK", resourceType: typeof(ButtonSkins));
-		public static ButtonSkin Open { get; } = new ButtonSkin(text: "Open", resourceType: typeof(ButtonSkins));
-		public static ButtonSkin Remove { get; } = new ButtonSkin(text: "Remove", resourceType: typeof(ButtonSkins));
-		public static ButtonSkin Save { get; } = new ButtonSkin(text: "Save", resourceType: typeof(ButtonSkins));
+		public static ButtonSkin Apply { get; } = new ButtonSkin(text: "Apply", icon: BootstrapIcon.Check2, resourceType: typeof(ButtonSkins));
+		public static ButtonSkin Cancel { get; } = new ButtonSkin(text: "Cancel", icon: BootstrapIcon.X, resourceType: typeof(ButtonSkins));
+		public static ButtonSkin Close { get; } = new ButtonSkin(text: "Close", icon: BootstrapIcon.X, resourceType: typeof(ButtonSkins));
+		public static ButtonSkin Delete { get; } = new ButtonSkin(text: "Delete", icon: BootstrapIcon.Trash, resourceType: typeof(ButtonSkins));
+		public static ButtonSkin Edit { get; } = new ButtonSkin(text: "Edit", icon: BootstrapIcon.Pencil, resourceType: typeof(ButtonSkins));
+		public static ButtonSkin Export { get; } = new ButtonSkin(text: "Export", icon: BootstrapIcon.BoxArrowUp, resourceType: typeof(ButtonSkins));
+		public static ButtonSkin Insert { get; } = new ButtonSkin(text: "Insert", icon: BootstrapIcon.BoxArrowInRight, resourceType: typeof(ButtonSkins));
+		public static ButtonSkin New { get; } = new ButtonSkin(text: "New", icon: BootstrapIcon.Plus, resourceType: typeof(ButtonSkins));
+		public static ButtonSkin OK { get; } = new ButtonSkin(text: "OK", icon: BootstrapIcon.Check2, resourceType: typeof(ButtonSkins));
+		public static ButtonSkin Open { get; } = new ButtonSkin(text: "Open", icon: BootstrapIcon.BoxArrowUpRight, resourceType: typeof(ButtonSkins));
+		public static ButtonSkin Remove { get; } = new ButtonSkin(text: "Remove", icon: BootstrapIcon.Trash, resourceType: typeof(ButtonSkins));
+		public static ButtonSkin Save { get; } = new ButtonSkin(text: "Save", icon: BootstrapIcon.Check2, resourceType: typeof(ButtonSkins));
 
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 	}
