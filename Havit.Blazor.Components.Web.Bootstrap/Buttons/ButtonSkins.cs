b5da852f90a62ce7f82f946f51e4eb6a79ca9a6f
Kanda, Jiří <kanda@havit.cz>
2020-07-22 09:56:31 +0200

    Dopady HxIcon (zapomenutý namespace v ButtonSkins).

--- Havit.Blazor.Components.Web.Bootstrap/Buttons/ButtonSkins.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Buttons/ButtonSkins.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Havit.Blazor.Components.Web.Bootstrap.Icons;
 
 namespace Havit.Blazor.Components.Web.Bootstrap.Buttons
 {
