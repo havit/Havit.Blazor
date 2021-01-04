ab3627b219bc4ad92f844043300e0af1610e3992
Robert Haken <haken@havit.cz>
2021-01-04 16:42:51 +0100

    fixes #53194: HxGrid - renderuje Footer, i když je prázdný

--- Havit.Blazor.Components.Web/RenderFragmentBuilder.cs
+++ Havit.Blazor.Components.Web/RenderFragmentBuilder.cs
@@ -12,7 +12,7 @@ namespace Havit.Blazor.Components.Web
 	public static class RenderFragmentBuilder
 	{
 		/// <summary>
-		/// Returns RenderFragment to render "nothing". Implementation returns null.
+		/// Returns RenderFragment to render "nothing". Implementation returns <c>null</c>.
 		/// </summary>
 		public static RenderFragment Empty()
 		{
@@ -21,9 +21,15 @@ namespace Havit.Blazor.Components.Web
 
 		/// <summary>
 		/// Returns RenderFragment which renders content and template (it is expected at least one of argument is null).		
+		/// If both are <c>null</c>, returns <see cref="Empty"/>.
 		/// </summary>
 		public static RenderFragment CreateFrom(string content, RenderFragment template)
 		{
+			if (content is null && template is null)
+			{
+				return Empty();
+			}
+
 			return (RenderTreeBuilder builder) =>
 			{
 				builder.AddContent(0, content); // null check: pokud je string null, použije prázdný řetězec
---
+++ Havit.Blazor.Components.WebTests/RenderFragmentBuilderTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bunit;
+using Havit.Blazor.Components.Web;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Havit.Blazor.Components.WebTests
+{
+	[TestClass]
+	public class RenderFragmentBuilderTests
+	{
+		[TestMethod]
+		public void RenderFragmentBuilder_Empty_ReturnsNull()
+		{
+			// act
+			var result = RenderFragmentBuilder.Empty();
+
+			// assert
+			Assert.IsNull(result);
+		}
+
+		[TestMethod]
+		public void RenderFragmentBuilder_CreateFrom_BothNull_ReturnsNull()
+		{
+			// act
+			var result = RenderFragmentBuilder.CreateFrom(null, null);
+
+			// assert
+			Assert.IsNull(result);
+		}
+
+		[TestMethod]
+		public void RenderFragmentBuilder_CreateFrom_BothSet_RendersContentFirst()
+		{
+			// assert
+			var ctx = new Bunit.TestContext();
+
+			// act
+			var result = ctx.Render(RenderFragmentBuilder.CreateFrom("content", (RenderTreeBuilder builder) => builder.AddContent(0, "template")));
+
+			// assert
+			result.MarkupMatches("contenttemplate");
+		}
+
+		[TestMethod]
+		public void RenderFragmentBuilder_CreateFrom_OnlyContentSet_RendersContent()
+		{
+			// assert
+			var ctx = new Bunit.TestContext();
+
+			// act
+			var result = ctx.Render(RenderFragmentBuilder.CreateFrom("content", null));
+
+			// assert
+			result.MarkupMatches("content");
+		}
+
+		[TestMethod]
+		public void RenderFragmentBuilder_CreateFrom_OnlyTemplateSet_RendersTemplate()
+		{
+			// assert
+			var ctx = new Bunit.TestContext();
+
+			// act
+			var result = ctx.Render(RenderFragmentBuilder.CreateFrom(null, (RenderTreeBuilder builder) => builder.AddContent(0, "template")));
+
+			// assert
+			result.MarkupMatches("template");
+		}
+
+		[TestMethod]
+		public void RenderFragmentBuilder_CreateFrom_EmptyStringContent_ReturnsFragmentWhichRendersStringEmpty()
+		{
+			// assert
+			var ctx = new Bunit.TestContext();
+
+			// act
+			var result = ctx.Render(RenderFragmentBuilder.CreateFrom(String.Empty, null));
+
+			// assert
+			result.MarkupMatches(String.Empty);
+		}
+	}
+}
