0bb691040883590bac66be7371d8fd0043112602
Jiří Kanda <kanda@havit.cz>
2021-01-07 19:43:47 +0100

    Task 52722: HxModal - implementace (work in progress)

---
+++ Havit.Blazor.Components.Web.Bootstrap/Modals/HxModal.cs
@@ -0,0 +1,87 @@
+﻿using Havit.Diagnostics.Contracts;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit.Blazor.Components.Web.Bootstrap
+{
+	// TODO: Jak se má správně renderovat HeaderTemplate?
+	// TODO: Uspořádání - jak umožnit použít "hezky" jakožto vlastní komponentu?
+	public partial class HxModal : ComponentBase, IAsyncDisposable
+	{
+		[Parameter] public string CssClass { get; set; }
+
+		[Parameter] public string Header { get; set; }
+
+		[Parameter] public RenderFragment HeaderTemplate { get; set; }
+
+		[Parameter] public RenderFragment BodyTemplate { get; set; }
+
+		[Parameter] public RenderFragment FooterTemplate { get; set; }
+
+		[Parameter] public bool ShowCloseButton { get; set; } = true;
+
+		[Parameter] public bool CloseOnEscape { get; set; } = true;
+
+		[Parameter] public bool UseStaticBackdrop { get; set; } = true;
+
+		[Parameter] public EventCallback Closed { get; set; }
+
+		[Inject] public IJSRuntime JSRuntime { get; set; }
+
+		private bool opened = false;
+		private bool shouldOpenModal = false;
+		DotNetObjectReference<HxModal> dotnetObjectReference;
+		private ElementReference modalElement;
+
+		public HxModal()
+		{
+			dotnetObjectReference = DotNetObjectReference.Create(this);
+		}
+
+		public Task ShowAsync()
+		{
+			Contract.Requires(!opened);
+
+			opened = true;
+			shouldOpenModal = true;
+
+			StateHasChanged();
+
+			return Task.CompletedTask;
+		}
+
+		protected override async Task OnAfterRenderAsync(bool firstRender)
+		{
+			await base.OnAfterRenderAsync(firstRender);
+			if (shouldOpenModal)
+			{
+				shouldOpenModal = false;
+				await JSRuntime.InvokeVoidAsync("hxModal_show", modalElement, dotnetObjectReference, UseStaticBackdrop, CloseOnEscape);
+			}
+		}
+
+		[JSInvokable]
+		public async Task HxModal_HandleModalHidden()
+		{
+			opened = false;
+			await Closed.InvokeAsync();
+		}
+
+		/// <inheritdoc />
+		public async ValueTask DisposeAsync()
+		{
+			if (opened)
+			{
+				// TODO: Má to význam? Asi ne (ničíme modal, který se zničí sám).
+				await JSRuntime.InvokeVoidAsync("hxModal_hide", modalElement);
+			}
+			dotnetObjectReference?.Dispose();
+		}
+	}
+}
--- Havit.Blazor.Components.Web.Bootstrap/wwwroot/hxbootstrap.js
+++ Havit.Blazor.Components.Web.Bootstrap/wwwroot/hxbootstrap.js
@@ -23,6 +23,31 @@ window.hxGrid_cellClick = (event) => {
     }
 }
 
+window.hxModal_show = (element, hxModalDotnetObjectReference, useStaticBackdrop, closeOnEscape) => {
+    element.hxModalDotnetObjectReference = hxModalDotnetObjectReference;
+    element.addEventListener('hidden.bs.modal', window.hxModal_handleModalHiddenInDotnet)
+
+    var modal = new bootstrap.Modal(element, {
+        backdrop: useStaticBackdrop ? "static" : false,
+        keyboard: closeOnEscape
+    });
+    modal.show();
+}
+
+window.hxModal_handleModalHiddenInDotnet = (event) => {
+    event.target.removeEventListener('hidden.bs.modal', window.hxModal_handleModalHiddenInDotnet);
+    event.target.hxModalDotnetObjectReference.invokeMethodAsync('HxModal_HandleModalHidden');
+    event.target.hxModalDotnetObjectReference = null;
+    hxModal_hide(event.target);
+};
+
+window.hxModal_hide = (element) =>
+{
+    var modal = bootstrap.Modal.getInstance(element);    
+    modal.hide();
+    modal.dispose();
+}
+
 var handleToastHiddenInDotnet = (event) => {
 	event.target.hxToastDotnetObjectReference.invokeMethodAsync('HxToast_HandleToastHidden');
 }; 
