f6f86729f322769196f389268eecbb46400a922f
Jiří Kanda <kanda@havit.cz>
2021-01-13 11:44:54 +0100

    Task 52722: HxModal - implementace (štábní kultura)

--- Havit.Blazor.Components.Web.Bootstrap/Modals/HxModal.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Modals/HxModal.cs
@@ -10,39 +10,98 @@ using System.Threading.Tasks;
 
 namespace Havit.Blazor.Components.Web.Bootstrap
 {
-	// TODO: Uspořádání - jak umožnit použít "hezky" jakožto vlastní komponentu?
-	public partial class HxModal : ComponentBase, IDisposable
+	/// <summary>
+	/// Modal component to render dialog as a Bootstrap Modal.
+	/// </summary>
+	public partial class HxModal : IDisposable
 	{
+		/// <summary>
+		/// Modal css class. Added to root div (.modal).
+		/// </summary>
 		[Parameter] public string CssClass { get; set; }
 
-		[Parameter] public string Header { get; set; }
+		#region Header properties
+		/// <summary>
+		/// Header text.
+		/// </summary>
+		[Parameter] public string HeaderText { get; set; }
 
+		/// <summary>
+		/// Header template.
+		/// </summary>
 		[Parameter] public RenderFragment HeaderTemplate { get; set; }
 
+		/// <summary>
+		/// Header css class.
+		/// </summary>
+		[Parameter] public string HeaderCssClass { get; set; }
+		#endregion
+
+		#region Body properties
+		/// <summary>
+		/// Body template.
+		/// </summary>
 		[Parameter] public RenderFragment BodyTemplate { get; set; }
 
+		/// <summary>
+		/// Body css class.
+		/// </summary>
+		[Parameter] public string BodyCssClass { get; set; }
+		#endregion
+
+		#region Footer properties
+		/// <summary>
+		/// Footer template.
+		/// </summary>
 		[Parameter] public RenderFragment FooterTemplate { get; set; }
 
+		/// <summary>
+		/// Footer css class.
+		/// </summary>
+		[Parameter] public string FooterCssClass { get; set; }
+		#endregion
+
+		/// <summary>
+		/// Indicates whether the modal shows close button in header.
+		/// Default value is true.
+		/// </summary>
 		[Parameter] public bool ShowCloseButton { get; set; } = true;
 
+		/// <summary>
+		/// Indicates whether the modal closes when escape key is pressed.
+		/// Default value is true.
+		/// </summary>
 		[Parameter] public bool CloseOnEscape { get; set; } = true;
 
+		/// <summary>
+		/// Indicates whether the modal uses a static backdrop.
+		/// Default value is true.
+		/// </summary>
 		[Parameter] public bool UseStaticBackdrop { get; set; } = true;
 
+		/// <summary>
+		/// Raised when modal is closed (whatever reason it is).
+		/// </summary>
 		[Parameter] public EventCallback OnClosed { get; set; }
 
-		[Inject] public IJSRuntime JSRuntime { get; set; }
+		[Inject] protected IJSRuntime JSRuntime { get; set; }
 
-		private bool opened = false;
-		private bool shouldOpenModal = false;
+		private bool opened = false; // indicates whether the modal is open
+		private bool shouldOpenModal = false; // indicates whether the modal is going to be opened
 		private DotNetObjectReference<HxModal> dotnetObjectReference;
 		private ElementReference modalElement;
 
+		/// <summary>
+		/// Constructor.
+		/// </summary>
 		public HxModal()
 		{
 			dotnetObjectReference = DotNetObjectReference.Create(this);
 		}
 
+		/// <summary>
+		/// Shows the modal.
+		/// </summary>
 		public Task ShowAsync()
 		{
 			Contract.Requires(!opened);
@@ -55,27 +114,34 @@ namespace Havit.Blazor.Components.Web.Bootstrap
 			return Task.CompletedTask;
 		}
 
+		/// <summary>
+		/// Hides the modal.
+		/// </summary>
 		public async Task HideAsync()
 		{
 			Contract.Requires(opened);
 			await JSRuntime.InvokeVoidAsync("hxModal_hide", modalElement);
 		}
 
-		[JSInvokable]
-		public async Task HxModal_HandleModalHidden()
+		[JSInvokable("HxModal_HandleModalHidden")]
+		public async Task HandleModalHidden()
 		{
 			opened = false;
 			await OnClosed.InvokeAsync(); // fires "event" dialog has been closed
 			StateHasChanged(); // ensures rerender to remove dialog from HTML
 		}
 
+		/// <inheritdoc />
 		protected override async Task OnAfterRenderAsync(bool firstRender)
 		{
 			await base.OnAfterRenderAsync(firstRender);
 
 			if (shouldOpenModal)
 			{
-				shouldOpenModal = false; // do not run hxModal_show in every render
+				// do not run hxModal_show in every render
+				// the line must be prior to JSRuntime (because BuildRenderTree/OnAfterRender[Async] is called twice; in the bad order of lines the JSRuntime would be also called twice).
+				shouldOpenModal = false;
+
 				// Running JS interop is postponed to OnAfterAsync to ensure modalElement is set.
 				await JSRuntime.InvokeVoidAsync("hxModal_show", modalElement, dotnetObjectReference, UseStaticBackdrop, CloseOnEscape);
 			}
