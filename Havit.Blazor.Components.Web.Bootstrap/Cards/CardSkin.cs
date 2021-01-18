6fc725cafc1562e3f96e1ce6e2d01a1d09d94041
Robert Haken <haken@havit.cz>
2021-01-18 15:58:57 +0100

    fixes #53447: HxCard - základní implementace se skiny

--- Havit.Blazor.Components.Web.Bootstrap/Cards/CardSkin.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Cards/CardSkin.cs
@@ -8,11 +8,19 @@ namespace Havit.Blazor.Components.Web.Bootstrap
 {
 	public class CardSkin
 	{
-		public static CardSkin DefaultSkin { get; set; }
+		/// <summary>
+		/// A skin to be used for any <see cref="HxCard"/> without explicit <see cref="HxCard.Skin"/> specified.
+		/// </summary>
+		public static CardSkin Default { get; set; }
+
+		/// <summary>
+		/// Empty skin to be able to disable <see cref="Default"/> locally.
+		/// </summary>
+		public static CardSkin None => new CardSkin();
 
 		/// <summary>
 		/// CSS class to be rendered with button.
 		/// </summary>
-		public string CssClass { get; }
+		public string CssClass { get; set; }
 	}
 }
