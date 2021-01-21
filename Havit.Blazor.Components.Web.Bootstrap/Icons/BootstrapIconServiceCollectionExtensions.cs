fb8d8ed31e9d1010a846d371f3bf8e409548f1aa
Jiří Kanda <kanda@havit.cz>
2021-01-21 09:58:58 +0100

    Bug 53326: bootstrap-icons.svg se při Server-renderingu načítá pro každou stránku znovu a znovu

---
+++ Havit.Blazor.Components.Web.Bootstrap/Icons/BootstrapIconServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit.Blazor.Components.Web.Bootstrap.Icons
+{
+	public static class BootstrapIconServiceCollectionExtensions
+	{
+		//public static IServiceCollection AddHxBootstrapIcons(this IServiceCollection services)
+		//{
+		//	services.ConfigureOptions<ConfigureStaticFileOptionsForBootstrapIcons>();
+		//	return services;
+		//}
+	}
+}
