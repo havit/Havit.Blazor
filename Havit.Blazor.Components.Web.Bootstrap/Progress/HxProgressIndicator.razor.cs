80959ad67378a945198cf881c7850f8b4daa23af
Jiří Kanda <kanda@havit.cz>
2021-01-22 14:04:44 +0100

    HxProgressIndicator

---
+++ Havit.Blazor.Components.Web.Bootstrap/Progress/HxProgressIndicator.razor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit.Blazor.Components.Web.Bootstrap.Progress
+{
+	public partial class HxProgressIndicator
+	{
+		// visual: https://nzbin.github.io/three-dots/
+		// source: https://github.com/nzbin/three-dots
+	}
+}
