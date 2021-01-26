05882142039e9a6acfb301855a78aff654a4f01c
Jiří Kanda <kanda@havit.cz>
2021-01-26 20:20:25 +0100

    HxBootstrap - používá icon font namísto svg

--- Havit.Blazor.Components.Web.Bootstrap/Icons/HxBootstrapIcon.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Icons/HxBootstrapIcon.cs
@@ -32,22 +32,11 @@ namespace Havit.Blazor.Components.Web.Bootstrap
 			// no base call
 
 			// https://icons.getbootstrap.com/
-			// <svg class="bi" width="32" height="32" fill="currentColor">
-			//   <use xlink:href="bootstrap-icons.svg#toggles"/>
-			// </svg>
 
-			builder.OpenElement(0, "svg");
-			builder.AddAttribute(1, "class", CssClassHelper.Combine("bi", CssClass));
-			builder.AddAttribute(2, "width", "1em");
-			builder.AddAttribute(3, "height", "1em");
-			builder.AddAttribute(4, "fill", "currentColor");
-			builder.AddMarkupContent(5, $"<use xlink:href=\"{GetIconHref()}\"></use>"); // does not work as OpenElement + AddAttribute
+			builder.OpenElement(0, "i");
+			builder.AddAttribute(1, "class", CssClassHelper.Combine("bi-" + Icon.Name, CssClass));
+
 			builder.CloseElement(); // svg
-		}
-
-		private string GetIconHref()
-		{
-			return "/_content/Havit.Blazor.Components.Web.Bootstrap/bootstrap-icons.svg#" + Icon.Name;
 		}
 	}
 }
