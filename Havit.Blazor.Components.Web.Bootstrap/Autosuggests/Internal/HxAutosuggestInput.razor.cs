9a4ca56fa9a6e069396ff8af16f2e6909a0fa509
Robert Haken <haken@havit.cz>
2021-02-01 16:05:13 +0100

    #52828 - HxAutosuggest - code-behind

---
+++ Havit.Blazor.Components.Web.Bootstrap/Autosuggests/Internal/HxAutosuggestInput.razor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Havit.Blazor.Components.Web.Bootstrap.Internal
+{
+	public partial class HxAutosuggestInput
+	{
+		[Parameter] public string Value { get; set; }
+
+		[Parameter] public EventCallback<string> OnInputInput { get; set; }
+
+		[Parameter] public EventCallback OnInputFocus { get; set; }
+
+		[Parameter] public EventCallback OnInputBlur { get; set; }
+
+		private async Task HandleInput(ChangeEventArgs changeEventArgs)
+		{
+			await OnInputInput.InvokeAsync((string)changeEventArgs.Value);
+		}
+
+		private async Task HandleFocus()
+		{
+			await OnInputFocus.InvokeAsync(null);
+		}
+
+		private async Task HandleBlur()
+		{
+			await OnInputBlur.InvokeAsync(null);
+		}
+	}
+}
---
+++ Havit.Blazor.Components.Web.Bootstrap/Autosuggests/Internal/HxAutosuggestItems.razor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Havit.Blazor.Components.Web.Bootstrap.Internal
+{
+	public partial class HxAutosuggestItems<TItemType>
+	{
+		[Parameter] public List<TItemType> Items { get; set; }
+
+		[Parameter] public EventCallback<TItemType> OnItemClick { get; set; }
+
+		/// <summary>
+		/// Selects text to display from item.
+		/// When not set ToString() is used.
+		/// </summary>
+		[Parameter] public Func<TItemType, string> TextSelector { get; set; }
+
+		private async Task HandleItemClick(TItemType value)
+		{
+			await OnItemClick.InvokeAsync(value);
+		}
+	}
+}
