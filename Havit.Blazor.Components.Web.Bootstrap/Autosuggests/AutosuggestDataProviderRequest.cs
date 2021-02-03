98cfc046915cd1059acd8ddd70c08c037fd100ec
Jiří Kanda <kanda@havit.cz>
2021-02-03 12:29:19 +0100

    Komentáře

--- Havit.Blazor.Components.Web.Bootstrap/Autosuggests/AutosuggestDataProviderRequest.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Autosuggests/AutosuggestDataProviderRequest.cs
@@ -2,10 +2,19 @@
 
 namespace Havit.Blazor.Components.Web.Bootstrap
 {
+	/// <summary>
+	/// Data provider request for autosuggest data.
+	/// </summary>
 	public class AutosuggestDataProviderRequest
 	{
+		/// <summary>
+		/// Autosuggest current user input.
+		/// </summary>
 		public string UserInput { get; init;  }
 
+		/// <summary>
+		/// The <see cref="System.Threading.CancellationToken"/> used to relay cancellation of the request.
+		/// </summary>
 		public CancellationToken CancellationToken { get; init; }
 	}
 }
--- Havit.Blazor.Components.Web.Bootstrap/Autosuggests/AutosuggestDataProviderResult.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Autosuggests/AutosuggestDataProviderResult.cs
@@ -2,8 +2,14 @@
 
 namespace Havit.Blazor.Components.Web.Bootstrap
 {
+	/// <summary>
+	/// Data provider result for autosuggest data.
+	/// </summary>
 	public class AutosuggestDataProviderResult<TItemType>
 	{
-		public List<TItemType> Items { get; set; }
+		/// <summary>
+		/// The provided items by the request.
+		/// </summary>
+		public IEnumerable<TItemType> Items { get; set; }
 	}
 }
