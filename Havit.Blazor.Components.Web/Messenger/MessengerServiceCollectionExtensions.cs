f0afb576343f705410cac813d4f21bc3e58d7d77
Robert Haken <haken@havit.cz>
2021-02-04 01:29:32 +0100

    .AddHxMessenger() - Singleton for WASM (1.0.2)

--- Havit.Blazor.Components.Web/Messenger/MessengerServiceCollectionExtensions.cs
+++ Havit.Blazor.Components.Web/Messenger/MessengerServiceCollectionExtensions.cs
@@ -18,7 +18,15 @@ namespace Havit.Blazor.Components.Web
 		/// </summary>
 		public static IServiceCollection AddHxMessenger(this IServiceCollection services)
 		{
-			return services.AddScoped<IHxMessengerService, HxMessengerService>();
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Create("BROWSER")))
+			{
+				// allows gRPC Interceptors and HttpMessageHandlers to pass error-messages to the HxMessenger without having to struggle with different DI Scope
+				return services.AddSingleton<IHxMessengerService, HxMessengerService>();
+			}
+			else
+			{
+				return services.AddScoped<IHxMessengerService, HxMessengerService>();
+			}
 		}
 	}
 }
