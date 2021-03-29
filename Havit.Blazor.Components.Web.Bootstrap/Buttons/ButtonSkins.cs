02cf18a23fb90c428e8cc120914baeeaac237662
Dominik Crha <crha@havit.cz>
2021-03-29 13:14:40 +0200

    Výměna tlačítek secondary za light

--- Havit.Blazor.Components.Web.Bootstrap/Buttons/ButtonSkins.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Buttons/ButtonSkins.cs
@@ -15,16 +15,16 @@ namespace Havit.Blazor.Components.Web.Bootstrap
 		// DO NOT FORGET TO MAINTAIN RESOURCES!
 
 		public static ButtonSkin Apply { get; set; } = new ButtonSkin() { Text = "Apply", ResourceType = typeof(ButtonSkins), Color = ThemeColor.Primary };
-		public static ButtonSkin Cancel { get; set; } = new ButtonSkin() { Text = "Cancel", ResourceType = typeof(ButtonSkins), Color = ThemeColor.Secondary };
-		public static ButtonSkin Close { get; set; } = new ButtonSkin() { Text = "Close", ResourceType = typeof(ButtonSkins), Color = ThemeColor.Secondary };
-		public static ButtonSkin Delete { get; set; } = new ButtonSkin() { Text = "Delete", ResourceType = typeof(ButtonSkins), Color = ThemeColor.Secondary };
-		public static ButtonSkin Edit { get; set; } = new ButtonSkin() { Text = "Edit", ResourceType = typeof(ButtonSkins), Color = ThemeColor.Secondary };
-		public static ButtonSkin Export { get; set; } = new ButtonSkin() { Text = "Export", ResourceType = typeof(ButtonSkins), Color = ThemeColor.Secondary };
+		public static ButtonSkin Cancel { get; set; } = new ButtonSkin() { Text = "Cancel", ResourceType = typeof(ButtonSkins), Color = ThemeColor.Light };
+		public static ButtonSkin Close { get; set; } = new ButtonSkin() { Text = "Close", ResourceType = typeof(ButtonSkins), Color = ThemeColor.Light };
+		public static ButtonSkin Delete { get; set; } = new ButtonSkin() { Text = "Delete", ResourceType = typeof(ButtonSkins), Color = ThemeColor.Light };
+		public static ButtonSkin Edit { get; set; } = new ButtonSkin() { Text = "Edit", ResourceType = typeof(ButtonSkins), Color = ThemeColor.Light };
+		public static ButtonSkin Export { get; set; } = new ButtonSkin() { Text = "Export", ResourceType = typeof(ButtonSkins), Color = ThemeColor.Light };
 		public static ButtonSkin Insert { get; set; } = new ButtonSkin() { Text = "Insert", ResourceType = typeof(ButtonSkins), Color = ThemeColor.Primary };
 		public static ButtonSkin New { get; set; } = new ButtonSkin() { Text = "New", ResourceType = typeof(ButtonSkins), Color = ThemeColor.Primary };
 		public static ButtonSkin OK { get; set; } = new ButtonSkin() { Text = "OK", ResourceType = typeof(ButtonSkins), Color = ThemeColor.Primary };
-		public static ButtonSkin Open { get; set; } = new ButtonSkin() { Text = "Open", ResourceType = typeof(ButtonSkins), Color = ThemeColor.Secondary };
-		public static ButtonSkin Remove { get; set; } = new ButtonSkin() { Text = "Remove", ResourceType = typeof(ButtonSkins), Color = ThemeColor.Secondary };
+		public static ButtonSkin Open { get; set; } = new ButtonSkin() { Text = "Open", ResourceType = typeof(ButtonSkins), Color = ThemeColor.Light };
+		public static ButtonSkin Remove { get; set; } = new ButtonSkin() { Text = "Remove", ResourceType = typeof(ButtonSkins), Color = ThemeColor.Light };
 		public static ButtonSkin Save { get; set; } = new ButtonSkin() { Text = "Save", ResourceType = typeof(ButtonSkins), Color = ThemeColor.Primary };
 		public static ButtonSkin Up { get; set; } = new ButtonSkin() { ResourceType = typeof(ButtonSkins), Color = ThemeColor.None, CssClass = "hx-grid-btn", Icon = BootstrapIcon.ChevronUp };
 		public static ButtonSkin Down { get; set; } = new ButtonSkin() { ResourceType = typeof(ButtonSkins), Color = ThemeColor.None, CssClass = "hx-grid-btn", Icon = BootstrapIcon.ChevronDown };
