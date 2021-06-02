c354407192228ac410e10da2da1f86d74bfccd81
Kanda, Jiří <kanda@havit.cz>
2021-06-02 10:45:25 +0200

    Task 55753: HxInputDateRange - komponenta pro vstup rozsahu data -  použití IStringLocalizer

---
+++ Havit.Blazor.Components.Web.Bootstrap/Forms/Dates/HxInputDateRangeInternal.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+
+namespace Havit.Blazor.Components.Web.Bootstrap.Internal
+{
+	public partial class HxInputDateRangeInternal
+	{
+		[Inject] public IStringLocalizer<HxInputDateRange> Localizer { get; set; }
+	}
+}
