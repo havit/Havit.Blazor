0bc07cdc90c93cc43790ec1d2975c3be1ab7e2ad
Robert Haken <haken@havit.cz>
2021-07-28 22:00:16 +0200

    #56573 HxOffcanvas - při otevírání zavřít případný aktuálně otevřený (více not supported)

--- Havit.Blazor.Components.Web.Bootstrap/wwwroot/hxoffcanvas.js
+++ Havit.Blazor.Components.Web.Bootstrap/wwwroot/hxoffcanvas.js
@@ -1,4 +1,8 @@
 ﻿export function show(element, hxOffcanvasDotnetObjectReference, backdropEnabled, closeOnEscape, scrollingEnabled) {
+	if (window.offcanvasElement) {
+		var previousOffcanvas = bootstrap.Offcanvas.getInstance(window.offcanvasElement);
+		previousOffcanvas.hide();
+	}
 	element.hxOffcanvasDotnetObjectReference = hxOffcanvasDotnetObjectReference;
 	element.addEventListener('hidden.bs.offcanvas', handleOffcanvasHidden)
 
@@ -6,13 +10,14 @@
 		backdrop: backdropEnabled,
 		keyboard: closeOnEscape,
 		scroll: scrollingEnabled
-    });
+	});
+	window.offcanvasElement = element;
     offcanvas.show();
 }
 
 export function hide(element) {
     var offcanvas = bootstrap.Offcanvas.getInstance(element);
-    offcanvas.hide();
+	offcanvas.hide();
 }
 
 export function dispose(element) {
@@ -27,6 +32,8 @@ function handleOffcanvasHidden(event) {
 	event.target.removeEventListener('hidden.bs.offcanvas', handleOffcanvasHidden);
 	event.target.hxOffcanvasDotnetObjectReference.invokeMethodAsync('HxOffcanvas_HandleOffcanvasHidden');
 	event.target.hxOffcanvasDotnetObjectReference = null;
+
+	window.offcanvasElement = null;
 
     var offcanvas = bootstrap.Offcanvas.getInstance(event.target);    
     offcanvas.dispose();
