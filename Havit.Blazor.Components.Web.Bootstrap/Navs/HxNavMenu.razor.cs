a35a77a5dff6c4391a1e398cc3be6d381392a04c
Michael Melena <melena@havit.cz>
2021-08-03 11:25:59 +0200

    #53148 Parametrizace HxNavMenu

--- Havit.Blazor.Components.Web.Bootstrap/Navs/HxNavMenu.razor.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Navs/HxNavMenu.razor.cs
@@ -4,28 +4,45 @@ namespace Havit.Blazor.Components.Web.Bootstrap
 {
 	public partial class HxNavMenu : ComponentBase
 	{
-		[Parameter] public RenderFragment ChildContent { get; set; }
 
-		[Parameter] public RenderFragment LoginDisplayContent { get; set; }
+
+
+
 
 		[Parameter] public string BrandName { get; set; }
 
 		[Parameter] public string BrandNameShort { get; set; }
 
-		private bool CollapsedMobile { get; set; } = true;
+		[Parameter] public RenderFragment BrandTemplate { get; set; }
+
+		[Parameter] public RenderFragment ItemsTemplate { get; set; }
+
+		[Parameter] public IconBase DesktopOpenCollapseIcon { get; set; } = BootstrapIcon.ChevronBarRight;
+
+		[Parameter] public IconBase DesktopCloseCollapseIcon { get; set; } = BootstrapIcon.ChevronBarLeft;
+
+		[Parameter] public IconBase LoginIcon { get; set; } = BootstrapIcon.Person;
+
+		[Parameter] public RenderFragment LoginDisplayTemplate { get; set; }
+
+		private bool CollapsedMobile { get; set; } = false;
+
 		private bool CollapsedDesktop { get; set; } = false;
 
 		private string GetCollapsedMobileCssClass() => CollapsedMobile ? "collapse" : null;
+
+		private string GetDesktopCollapsedCssClass() => CollapsedDesktop ? "desktop-collapse" : null;
 
 		private void ToggleMobileNavMenu()
 		{
 			CollapsedMobile = !CollapsedMobile;
 			CollapsedDesktop = false;
 		}
+
 		private void ToggleDesktopNavMenu()
 		{
 			CollapsedDesktop = !CollapsedDesktop;
-			CollapsedMobile = true;
+			CollapsedMobile = false;
 		}
 	}
 }
