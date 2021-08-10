35f931919d64a7a3a82413a84b5cd3adac5f1a1d
Robert Haken <haken@havit.cz>
2021-08-10 09:23:00 +0200

    HxAnchorFragmentNavigation.js - logging removed

--- Havit.Blazor.Components.Web.Bootstrap/wwwroot/HxAnchorFragmentNavigation.js
+++ Havit.Blazor.Components.Web.Bootstrap/wwwroot/HxAnchorFragmentNavigation.js
@@ -1,20 +1,14 @@
 ﻿export function scrollToAnchor(anchor) {
 	var selector = anchor || document.location.hash;
-	console.log("selector:" + selector);
 	if (selector && (selector.length > 1)) {
 		var element = document.querySelector(selector);
 		if (element) {
-
-			console.log("scrollIntoView");
-
 			element.scrollIntoView({
 				behavior: 'smooth'
 			});
 		}
 	}
 	else {
-		console.log("scroll(0,0)");
-
 		window.scroll(0, 0);
 	}
 }
