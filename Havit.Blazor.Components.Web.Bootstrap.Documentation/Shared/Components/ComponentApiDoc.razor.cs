3e7934c39e8a9f0bcca5ba02e3a1f0d9d03f19b1
Mikuláš Hoblík <mikuhoblik@gmail.com>
2021-08-16 09:23:57 +0200

    #56653 ComponentApiDoc - dočištění implementace

--- Havit.Blazor.Components.Web.Bootstrap.Documentation/Shared/Components/ComponentApiDoc.razor.cs
+++ Havit.Blazor.Components.Web.Bootstrap.Documentation/Shared/Components/ComponentApiDoc.razor.cs
@@ -55,6 +55,11 @@ namespace Havit.Blazor.Components.Web.Bootstrap.Documentation.Shared.Components
 
 		[Inject] private NavigationManager NavigationManager { get; set; }
 
+		private static readonly List<string> byDefaultExcludedProperties = new() { "Defaults", "JSRuntime" };
+
+		private static readonly List<string> objectDerivedMethods = new() { "ToString", "GetType", "Equals", "GetHashCode" };
+		private static readonly List<string> derivedMethods = new() { "Dispose", "DisposeAsync", "SetParametersAsync", "ChildContent" };
+
 		private static readonly HttpClient client = new HttpClient();
 
 		private ClassMember classMember;
@@ -168,7 +173,6 @@ namespace Havit.Blazor.Components.Web.Bootstrap.Documentation.Shared.Components
 		private bool DetermineWhetherPropertyShouldBeAdded(Property property)
 		{
 			string name = property.PropertyInfo.Name;
-			List<string> byDefaultExcludedProperties = new() { "Defaults", "JSRuntime" };
 			if (byDefaultExcludedProperties.Contains(name) || ExcludedMembers.Contains(name))
 			{
 				return false;
@@ -190,8 +194,6 @@ namespace Havit.Blazor.Components.Web.Bootstrap.Documentation.Shared.Components
 			}
 
 			string name = method.MethodInfo.Name;
-			List<string> objectDerivedMethods = new() { "ToString", "GetType", "Equals", "GetHashCode" };
-			List<string> derivedMethods = new() { "Dispose", "DisposeAsync", "SetParametersAsync", "ChildContent" };
 			if (name.Contains("set") || name.Contains("get") || objectDerivedMethods.Contains(name) || derivedMethods.Contains(name) || ExcludedMembers.Contains(name))
 			{
 				return false;
@@ -215,7 +217,7 @@ namespace Havit.Blazor.Components.Web.Bootstrap.Documentation.Shared.Components
 
 		private string GetDownloadLink()
 		{
-			if (string.IsNullOrEmpty(Type.Namespace) == true)
+			if (string.IsNullOrEmpty(Type.Namespace))
 			{
 				return $"{NavigationManager.BaseUri}Havit.Blazor.Components.Web.Bootstrap.xml";
 			}
@@ -237,237 +239,6 @@ namespace Havit.Blazor.Components.Web.Bootstrap.Documentation.Shared.Components
 		private string GetBootstrapComponentName()
 		{
 			return Type.Name.Replace("Hx", "");
-		}
-
-		public class Member
-		{
-			protected string TryFormatComment(string comment)
-			{
-				try
-				{
-					if (string.IsNullOrEmpty(comment))
-					{
-						return string.Empty;
-					}
-
-					// <c>
-					{
-						Regex regex = new("<c>");
-						comment = regex.Replace(comment, "<code>");
-						regex = new("</c>");
-						comment = regex.Replace(comment, "</code>");
-					}
-
-					// <see cref=""/>
-					{
-						Regex regex = new("<see");
-						comment = regex.Replace(comment, "<a");
-
-						regex = new("</see>");
-						comment = regex.Replace(comment, "</a>");
-
-						regex = new("cref=\"([A-Za-z\\.:])+");
-						var matches = regex.Matches(comment);
-
-						foreach (var match in matches)
-						{
-							string link = match.ToString().Split('\"').LastOrDefault(); // get the part in the quotes (value of the cref attribute)
-							string[] splitLink = link.Split('.');
-
-							regex = new("cref=\"([A-Za-z\\.:`\\d])+\" ?/>"); // find this part of the element (beggining already replaced): cref="P:System.Text.Regex.Property" />
-							comment = regex.Replace(comment, GenerateFullLink(splitLink), 1); // replace the above with a generated link to the documentation
-						}
-					}
-				}
-				catch
-				{
-					// NOOP
-				}
-
-				return comment;
-			}
-
-			private string GenerateFullLink(string[] splitLink)
-			{
-				if (splitLink is null || splitLink.Length == 0)
-				{
-					return string.Empty;
-				}
-
-				if (splitLink[0].Contains("Havit"))
-				{
-					return GenerateHavitDocumentationLink(splitLink);
-				}
-
-				if (splitLink[0].Contains("Microsoft") || splitLink[0].Contains("System"))
-				{
-					return GenerateMicrosoftDocumentationLink(splitLink);
-				}
-
-				throw new NotSupportedException();
-			}
-
-			private string GenerateHavitDocumentationLink(string[] splitLink)
-			{
-				bool containsHx = false;
-				string seeName = "";
-
-				string fullLink = "";
-
-				bool isType = IsType(splitLink);
-
-				if (isType)
-				{
-					for (int i = splitLink.Length - 1; i >= 0; i--) // loop through the array from the back, prepend the current part, and stop once the component name is reached
-					{
-						fullLink = $"{splitLink[i]}/{fullLink}";
-
-						if (splitLink[i].Contains("Hx"))
-						{
-							containsHx = true;
-							seeName = splitLink[i];
-							break;
-						}
-					}
-				}
-				else
-				{
-					if (splitLink.Length >= 2)
-					{
-						fullLink = splitLink[^2];
-						seeName = $"{splitLink[^2]}.{splitLink[^1]}";
-					}
-				}
-
-				// Default case: if "Hx" wasn't found anywhere, it couldn't be detected what part to use in the link, so the default part will be chosen
-				if (fullLink.Length == 0 || (containsHx == false && isType))
-				{
-					fullLink = splitLink[^1];
-					seeName = splitLink[^1];
-				}
-
-				fullLink = $"href=\"/components/{fullLink}";
-				fullLink += $"\">{seeName}</a>";
-				return fullLink;
-			}
-
-			private string GenerateMicrosoftDocumentationLink(string[] splitLink)
-			{
-				bool isType = IsType(splitLink);
-
-				string fullLink = "";
-				string seeName = splitLink[^1];
-				if (isType)
-				{
-					string link = ConcatenateStringArray(splitLink);
-					fullLink = link.Split(':')[^1];
-				}
-
-				fullLink = $"href=\"https://docs.microsoft.com/en-us/dotnet/api/{fullLink}";
-				fullLink += $"\">{seeName}</a>";
-				return fullLink;
-			}
-
-			private bool IsType(string[] splitLink)
-			{
-				bool isType = false;
-				if (splitLink[0][0] == 'T')
-				{
-					isType = true;
-				}
-
-				return isType;
-			}
-
-			private string ConcatenateStringArray(string[] stringArray)
-			{
-				StringBuilder sb = new StringBuilder();
-				foreach (var text in stringArray)
-				{
-					sb.Append(text);
-					sb.Append(".");
-				}
-
-				sb.Remove(sb.Length - 1, 1);
-
-				return sb.ToString();
-			}
-		}
-
-		public class ClassMember : Member
-		{
-			public TypeComments Comments
-			{
-				get => comments;
-				set
-				{
-					TypeComments inputComments = value;
-					inputComments.Summary = TryFormatComment(inputComments.Summary);
-					comments = inputComments;
-				}
-			}
-			private TypeComments comments;
-		}
-
-		public class Property : Member
-		{
-			public PropertyInfo PropertyInfo { get; set; }
-
-			public CommonComments Comments
-			{
-				set
-				{
-					CommonComments inputComments = value;
-					try { inputComments.Summary = TryFormatComment(inputComments.Summary); } catch { }
-					comments = inputComments;
-				}
-				get
-				{
-					return comments;
-				}
-			}
-			private CommonComments comments;
-		}
-
-		public class Method : Member
-		{
-			public MethodInfo MethodInfo { get; set; }
-
-			public MethodComments Comments
-			{
-				set
-				{
-					MethodComments inputComments = value;
-					inputComments.Summary = TryFormatComment(inputComments.Summary);
-					comments = inputComments;
-				}
-				get
-				{
-					return comments;
-				}
-			}
-			private MethodComments comments;
-
-			public string GetParameters()
-			{
-				StringBuilder concatenatedParameters = new StringBuilder();
-				var parameters = MethodInfo.GetParameters();
-
-				if (parameters is null || parameters.Length == 0)
-				{
-					return "()";
-				}
-
-				concatenatedParameters.Append("(");
-				foreach (var parameter in parameters)
-				{
-					concatenatedParameters.Append($"{FormatType(parameter.ParameterType)} {parameter.Name}, ");
-				}
-				concatenatedParameters.Remove(concatenatedParameters.Length - 2, 2);
-				concatenatedParameters.Append(")");
-
-				return concatenatedParameters.ToString();
-			}
 		}
 
 		private static string ReplaceTypeNames(string type)
