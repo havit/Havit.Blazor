992c12b3101d7b6284150f618a1b86cd103e82cb
Robert Haken <haken@havit.cz>
2021-08-16 12:35:01 +0200

    HxInputTags WIP (10%)

---
+++ Havit.Blazor.Components.Web.Bootstrap/Tags/HxInputTags.razor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Havit.Blazor.Components.Web.Bootstrap
+{
+	public partial class HxInputTags
+	{
+		[Parameter] public AutosuggestDataProviderDelegate<string> NewItemSuggestionsDataProvider { get; set; }
+	}
+}
--- Havit.Blazor.Components.Web.Bootstrap/Tags/HxTag.razor.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Tags/HxTag.razor.cs
@@ -7,8 +7,34 @@ using Microsoft.AspNetCore.Components;
 
 namespace Havit.Blazor.Components.Web.Bootstrap
 {
+	/// <summary>
+	/// Single tag visual. Building block for <see cref="HxInputTags"/>.
+	/// </summary>
 	public partial class HxTag
 	{
+		/// <summary>
+		/// Tag background color. Default is <see cref="ThemeColor.Light"/>.
+		/// </summary>
+		[Parameter] public ThemeColor BackgroundColor { get; set; } = ThemeColor.Light;
+
+		/// <summary>
+		/// Tag text (value) color. Default is <see cref="ThemeColor.Dark"/>.
+		/// </summary>
+		[Parameter] public ThemeColor TextColor { get; set; } = ThemeColor.Dark;
+
+		/// <summary>
+		/// Any additional CSS class to apply.
+		/// </summary>
+		[Parameter] public string CssClass { get; set; }
+
+		/// <summary>
+		/// Tag text (value).
+		/// </summary>
 		[Parameter] public string Text { get; set; }
+
+		/// <summary>
+		/// Raised when the remove button is clicked.
+		/// </summary>
+		[Parameter] public EventCallback OnRemove { get; set; }
 	}
 }
