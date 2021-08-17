a1317af013cfd09def7aa91a06e36dfc1e44d034
Robert Haken <haken@havit.cz>
2021-08-17 10:55:11 +0200

    Havit.Blazor.Grpc.Client.WebAssembly - AuthorizationGrpcClientInterceptor (release)

---
+++ Havit.Blazor.Grpc.Client.WebAssembly/AuthorizationGrpcClientInterceptor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core.Interceptors;
+using Grpc.Core;
+using static Grpc.Core.Interceptors.Interceptor;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+namespace Havit.Blazor.Grpc.Client.WebAssembly
+{
+	/// <summary>
+	/// gRPC Client Interceptor which redirects UI to login when access token is not available (gRPC call to service which requires auth).
+	/// </summary>
+	public class AuthorizationGrpcClientInterceptor : Interceptor
+	{
+		public override AsyncUnaryCall<TResponse> AsyncUnaryCall<TRequest, TResponse>(
+					TRequest request,
+					ClientInterceptorContext<TRequest, TResponse> context,
+					AsyncUnaryCallContinuation<TRequest, TResponse> continuation)
+		{
+			var call = continuation(request, context);
+
+			return new AsyncUnaryCall<TResponse>(HandleAsyncUnaryResponse(call.ResponseAsync), call.ResponseHeadersAsync, call.GetStatus, call.GetTrailers, call.Dispose);
+		}
+
+		private async Task<TResponse> HandleAsyncUnaryResponse<TResponse>(Task<TResponse> inputResponse)
+		{
+			try
+			{
+				return await inputResponse;
+			}
+			catch (RpcException e) when (e.Status.DebugException is AccessTokenNotAvailableException innerException)
+			{
+				innerException.Redirect();
+
+				return default;
+			}
+		}
+	}
+}
