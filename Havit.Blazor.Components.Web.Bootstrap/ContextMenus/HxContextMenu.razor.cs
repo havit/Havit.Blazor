4ac0f199e4e44eda81c13ac226c077e6831e8608
Robert Haken <haken@havit.cz>
2021-09-02 16:18:14 +0200

    HxContextMenu doc

--- Havit.Blazor.Components.Web.Bootstrap/ContextMenus/HxContextMenu.razor.cs
+++ Havit.Blazor.Components.Web.Bootstrap/ContextMenus/HxContextMenu.razor.cs
@@ -5,6 +5,9 @@ using Microsoft.AspNetCore.Components;
 
 namespace Havit.Blazor.Components.Web.Bootstrap
 {
+	/// <summary>
+	/// Ready-made context menu (based on <see href="https://getbootstrap.com/docs/5.1/components/dropdowns/">Bootstrap Dropdown</see>).
+	/// </summary>
 	public partial class HxContextMenu
 	{
 		[Parameter] public RenderFragment ChildContent { get; set; }
