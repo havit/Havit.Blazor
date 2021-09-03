ec3361e5958c9846c1647c005560c85556a46ff8
Mikuláš Hoblík <mikuhoblik@gmail.com>
2021-09-03 17:12:08 +0200

    #57203 HxListLayoutTest - rozšířit komplexitu testu

--- BlazorAppTest/Pages/HxListLayoutTest.razor.cs
+++ BlazorAppTest/Pages/HxListLayoutTest.razor.cs
@@ -19,7 +19,7 @@ namespace BlazorAppTest.Pages
 
 		private readonly IEnumerable<NamedView<FilterModelDto>> namedViews = new List<NamedView<FilterModelDto>>()
 		{
-			new NamedView<FilterModelDto>("Minimum = 1", () => new FilterModelDto { MinimumItemId = 1 } ),
+			new NamedView<FilterModelDto>("Minimum = 1", () => new FilterModelDto { MinimumItemId = 1 }),
 			new NamedView<FilterModelDto>("Minimum = 2", () => new FilterModelDto { MinimumItemId = 2 }),
 			new NamedView<FilterModelDto>("Minimum = 3", () => new FilterModelDto { MinimumItemId = 3 })
 		};
@@ -28,12 +28,24 @@ namespace BlazorAppTest.Pages
 		{
 			List<DataItemDto> data = new List<DataItemDto>()
 			{
-				new DataItemDto() { ItemId = 1, Name = "Jednička"},
-				new DataItemDto() { ItemId = 2, Name = "Dvojka"},
-				new DataItemDto() { ItemId = 3, Name = "Trojka"}
+				new DataItemDto() { ItemId = 1, Name = "Edward P. Lopez", Email="EdwardPLopez@teleworm.us", PhoneNumber="918-483-4067", Age=45},
+				new DataItemDto() { ItemId = 2, Name = "Stanley M. Bolduc", Email="StanleyMBolduc@armyspy.com", PhoneNumber="330-510-7016", Age=29},
+				new DataItemDto() { ItemId = 3, Name = "David D. Lounsbury", Email="DavidDLounsbury@armyspy.com", PhoneNumber="212-292-4140", Age=38},
+				new DataItemDto() { ItemId = 4, Name = "Benjamin M. Damato", Email="BenjaminMDamato@jourrapide.com", PhoneNumber="225-268-0659", Age=63},
+				new DataItemDto() { ItemId = 5, Name = "Carol B. Jhonson", Email="CarolBJhonson@teleworm.us", PhoneNumber="217-243-3921", Age=25},
+				new DataItemDto() { ItemId = 6, Name = "Juan I. Nichols", Email="JuanINichols@rhyta.com", PhoneNumber="727-562-0918", Age=60}
 			};
 
 			IEnumerable<DataItemDto> result = data.Where(i => i.ItemId >= filterModel.MinimumItemId).ToList();
+			if (filterModel.NameContains is not null)
+			{
+				result = result.Where(i => i.Name.Contains(filterModel.NameContains));
+			}
+			result = result.Where(i => i.Age >= filterModel.MinimumAge);
+			if (filterModel.MaximumAge > filterModel.MinimumAge)
+			{
+				result = result.Where(i => i.Age <= filterModel.MaximumAge);
+			}
 
 			return Task.FromResult(new GridDataProviderResult<DataItemDto>()
 			{
@@ -82,12 +94,18 @@ namespace BlazorAppTest.Pages
 		public record FilterModelDto
 		{
 			public int MinimumItemId { get; set; }
+			public string NameContains { get; set; }
+			public int MinimumAge { get; set; }
+			public int MaximumAge { get; set; }
 		}
 
 		public record DataItemDto
 		{
 			public int ItemId { get; set; }
 			public string Name { get; set; }
+			public string Email { get; set; }
+			public string PhoneNumber { get; set; }
+			public int Age { get; set; }
 		}
 	}
 }
