70f2f4750047022894be1ff6f1c4bb7f1d8cfdd8
Dominik Crha <crha@havit.cz>
2021-09-08 20:13:37 +0200

    Docs - Texarea link

--- Havit.Blazor.Components.Web.Bootstrap/Forms/HxInputTextArea.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Forms/HxInputTextArea.cs
@@ -7,8 +7,7 @@ using Microsoft.AspNetCore.Components.Rendering;
 namespace Havit.Blazor.Components.Web.Bootstrap
 {
 	/// <summary>
-	/// Textarea.
-	/// https://getbootstrap.com/docs/5.0/forms/floating-labels/#textareas
+	/// <a href="https://getbootstrap.com/docs/5.0/forms/floating-labels/#textareas" target="_blank">Textarea</a>.
 	/// To set a custom height, do not use the rows attribute. Instead, set an explicit height (either inline or via custom CSS).
 	/// </summary>
 	public class HxInputTextArea : HxInputText
