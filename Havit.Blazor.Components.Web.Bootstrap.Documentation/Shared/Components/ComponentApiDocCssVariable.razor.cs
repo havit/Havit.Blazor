87fbeddd1569420f90de95ffcb75296ead285052
Mikuláš Hoblík <mikuhoblik@gmail.com>
2021-09-08 17:54:56 +0200

    #57413 ComponentApiDoc - CSS Variables renderovat do tabulky

--- Havit.Blazor.Components.Web.Bootstrap.Documentation/Shared/Components/ComponentApiDocCssVariable.razor.cs
+++ Havit.Blazor.Components.Web.Bootstrap.Documentation/Shared/Components/ComponentApiDocCssVariable.razor.cs
@@ -8,7 +8,14 @@ namespace Havit.Blazor.Components.Web.Bootstrap.Documentation.Shared.Components
 {
 	public partial class ComponentApiDocCssVariable
 	{
+		/// <summary>
+		/// Name of the css variable.
+		/// </summary>
 		[Parameter] public string Name { get; set; }
+		/// <summary>
+		/// Default value for the css variable.
+		/// </summary>
+		[Parameter] public string Default { get; set; }
 		[Parameter] public RenderFragment ChildContent { get; set; }
 	}
 }
