d5fafdf8fc7f7dd1d543d23bbe17cb3ea9f4ed72
Robert Haken <haken@havit.cz>
2021-09-17 12:33:57 +0200

    #57515 Doc - Titles (server prerendering)

---
+++ Havit.Blazor.Components.Web.Bootstrap.Documentation.Server/Pages/_Host.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Havit.Blazor.Components.Web.Bootstrap.Documentation.Server.Pages
+{
+	public class _Host : PageModel
+	{
+		private const string TitleBase = "HAVIT Blazor Documentation";
+		private const string TitleSeparator = " | ";
+
+		public string Title { get; set; }
+
+		public void OnGet()
+		{
+			var path = this.Request.Path.ToString().TrimEnd('/');
+
+			var lastSegmentStart = path.LastIndexOf("/");
+			if (lastSegmentStart > 0)
+			{
+				Title = path.Substring(lastSegmentStart + 1) + TitleSeparator + TitleBase;
+				return;
+
+			}
+
+			Title = TitleBase + TitleSeparator + "Free Bootstrap 5 Blazor components";
+		}
+
+	}
+}
