0b3eb96148b490ccef8dd974fdfe0d0df2dda2f2
Mikuláš Hoblík <mikuhoblik@gmail.com>
2021-09-22 18:05:00 +0200

    #57567 HxBreadcrumb - implement

---
+++ Havit.Blazor.Components.Web.Bootstrap/Navigation/HxBreadcrumb/HxBreadcrumb.razor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Havit.Blazor.Components.Web.Bootstrap
+{
+	public partial class HxBreadcrumb
+	{
+		/// <summary>
+		/// Child content.
+		/// </summary>
+		[Parameter] public RenderFragment ChildContent { get; set; }
+		/// <summary>
+		/// Breadcrumb divider. Default is <c>/</c>. Enter either a character (such as <c>></c>) or use an embedded SVG icon. Disable the divider with the value <c>none</c>.
+		/// </summary>
+		[Parameter] public string Divider { get; set; }
+	}
+}
---
+++ Havit.Blazor.Components.Web.Bootstrap/Navigation/HxBreadcrumb/HxBreadcrumbItem.razor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Havit.Blazor.Components.Web.Bootstrap
+{
+	public partial class HxBreadcrumbItem
+	{
+		/// <summary>
+		/// Determines whether the <c>HxBreadcrumbItem</c> is active (use for a page that the user is currently on).
+		/// </summary>
+		[Parameter] public bool Active { get; set; }
+		/// <summary>
+		/// The link of the breadcrumb (a page where the user will be led on click).
+		/// </summary>
+		[Parameter] public string Link { get; set; }
+		/// <summary>
+		/// Text of the link (usually a name of the page).
+		/// </summary>
+		[Parameter] public string Text { get; set; }
+	}
+}
