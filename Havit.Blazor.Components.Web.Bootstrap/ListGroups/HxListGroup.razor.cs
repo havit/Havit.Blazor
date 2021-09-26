e270efbce014f910e44d4d0e04d532553532a070
Robert Haken <haken@havit.cz>
2021-09-26 02:21:12 +0200

    HxListGroup - basic cleanup

--- Havit.Blazor.Components.Web.Bootstrap/ListGroups/HxListGroup.razor.cs
+++ Havit.Blazor.Components.Web.Bootstrap/ListGroups/HxListGroup.razor.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 
 namespace Havit.Blazor.Components.Web.Bootstrap
 {
 	/// <summary>
+	/// <see href="https://getbootstrap.com/docs/5.1/components/list-group/">Bootstrap 5 List group</see> component.<br/>
 	/// List groups are a flexible and powerful component for displaying a series of content.
 	/// </summary>
 	public partial class HxListGroup
@@ -18,22 +14,22 @@ namespace Havit.Blazor.Components.Web.Bootstrap
 		[Parameter] public RenderFragment ChildContent { get; set; }
 
 		/// <summary>
-		/// If set to true, removes borders and rounded corners to render list group items edge-to-edge.
+		/// If set to <c>true</c>, removes borders and rounded corners to render list group items edge-to-edge.
 		/// </summary>
 		[Parameter] public bool Flush { get; set; }
 
 		/// <summary>
-		/// Set to true to opt into numbered list group items. The list group changes from an unordered list to an ordered list.
+		/// Set to <c>true</c> to opt into numbered list group items. The list group changes from an unordered list to an ordered list.
 		/// </summary>
 		[Parameter] public bool Numbered { get; set; }
 
 		/// <summary>
-		/// If true, changes the layout of the list group items from vertical to horizontal across all breakpoints. Cannot be combined with flush list groups.
+		/// If <c>true</c>, changes the layout of the list group items from vertical to horizontal across all breakpoints. Cannot be combined with flush list groups.
 		/// </summary>
 		[Parameter] public bool Horizontal { get; set; }
 
 		/// <summary>
-		/// If true, items will have equal width. Use only when the <code>HxListGroup</code> is horizontal.
+		/// If <c>true</c>, items will have equal width. Use only when the <code>HxListGroup</code> is horizontal.
 		/// </summary>
 		[Parameter] public bool EqualWidthItems { get; set; }
 
--- Havit.Blazor.Components.Web.Bootstrap/ListGroups/HxListGroupItem.razor.cs
+++ Havit.Blazor.Components.Web.Bootstrap/ListGroups/HxListGroupItem.razor.cs
@@ -1,16 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 
 namespace Havit.Blazor.Components.Web.Bootstrap
 {
+	/// <summary>
+	/// Item for <see cref="HxListGroup"/>.
+	/// </summary>
 	public partial class HxListGroupItem
 	{
 		/// <summary>
-		/// Content of the <code>HxListGroupItem</code>.
+		/// Content of the item.
 		/// </summary>
 		[Parameter] public RenderFragment ChildContent { get; set; }
 
@@ -20,14 +18,10 @@ namespace Havit.Blazor.Components.Web.Bootstrap
 		[Parameter] public bool Active { get; set; }
 
 		/// <summary>
-		/// Make the item appear disabled.
+		/// Make the item appear disabled by setting to <c>false</c>.
+		/// Default is <c>true</c>.
 		/// </summary>
-		[Parameter] public bool Disabled { get; set; }
-
-		/// <summary>
-		/// Whether the list item should act as a button.
-		/// </summary>
-		[Parameter] public bool Button { get; set; }
+		[Parameter] public bool Enabled { get; set; } = true;
 
 		/// <summary>
 		/// Color.
@@ -39,29 +33,17 @@ namespace Havit.Blazor.Components.Web.Bootstrap
 		/// </summary>
 		[Parameter] public EventCallback OnClick { get; set; }
 
-		[CascadingParameter] private HxListGroup listGroup { get; set; }
-
-		private bool flexFill;
-
-		protected override void OnParametersSet()
-		{
-			flexFill = listGroup.EqualWidthItems;
-		}
-
-		private void InvokeOnClick()
-		{
-			OnClick.InvokeAsync();
-		}
+		[CascadingParameter] protected HxListGroup ListGroupContainer { get; set; }
 
 		private string GetClasses()
 		{
 			return CssClassHelper.Combine(
 				"list-group-item",
 				Active ? "active" : null,
-				Disabled ? "disabled" : null,
-				Button ? "list-group-item-action" : null,
+				Enabled ? null : "disabled",
+				OnClick.HasDelegate ? "list-group-item-action" : null,
 				Color is not null ? GetColorCss() : null,
-				flexFill ? "flex-fill" : null);
+				ListGroupContainer.EqualWidthItems ? "flex-fill" : null);
 		}
 
 		private string GetColorCss()
