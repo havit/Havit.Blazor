aed084d6b7062667bd5653805b59f72b3c48e655
Mikuláš Hoblík <mikuhoblik@gmail.com>
2021-09-29 06:59:19 +0200

    #57630 HxProgress - basic implementation + documentation

---
+++ Havit.Blazor.Components.Web.Bootstrap/Progress/HxProgress.razor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Havit.Blazor.Components.Web.Bootstrap
+{
+	/// <summary>
+	/// <see href="https://getbootstrap.com/docs/5.1/components/progress/">Bootstrap 5 Progress</see> component.<br/>
+	/// A wrapping component for the <see cref="HxProgressBar" />.
+	/// </summary>
+	public partial class HxProgress
+	{
+		/// <summary>
+		/// Content consisting of one or multiple <c>HxProgressBar</c> components.
+		/// </summary>
+		[Parameter] public RenderFragment ChildContent { get; set; }
+
+		/// <summary>
+		/// Additional CSS classes for the progress.
+		/// </summary>
+		[Parameter] public string CssClass { get; set; }
+
+		/// <summary>
+		/// Height of all inner progress bars. Default is <c>15</c> pixels.
+		/// </summary>
+		[Parameter] public int Height { get; set; } = 15;
+	}
+}
---
+++ Havit.Blazor.Components.Web.Bootstrap/Progress/HxProgressBar.razor.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Havit.Blazor.Components.Web.Bootstrap
+{
+	/// <summary>
+	/// A progress bar to be placed inside <see cref="HxProgress" />.
+	/// </summary>
+	public partial class HxProgressBar
+	{
+		/// <summary>
+		/// Inner content of the progress bar.
+		/// </summary>
+		[Parameter] public RenderFragment ChildContent { get; set; }
+
+		/// <summary>
+		/// Additional CSS classes to be applied to the progress bar.
+		/// </summary>
+		[Parameter] public string CssClass { get; set; }
+
+		/// <summary>
+		/// Current value (proportion of the progress bar that is taken up).
+		/// </summary>
+		[Parameter] public int Value { get; set; }
+
+		/// <summary>
+		/// Lowest possible value. Default is <c>0</c>.
+		/// </summary>
+		[Parameter] public int MinValue { get; set; } = 0;
+
+		/// <summary>
+		/// Highest possible value. Default is <c>100</c>.
+		/// </summary>
+		[Parameter] public int MaxValue { get; set; } = 100;
+
+		/// <summary>
+		/// Text to be displayed on the progress bar.
+		/// </summary>
+		[Parameter] public string Label { get; set; }
+
+		/// <summary>
+		/// Fill (background) color of the progress bar.
+		/// </summary>
+		[Parameter] public ThemeColor? Color { get; set; }
+
+		/// <summary>
+		/// If <c>true</c>, applies a stripe via CSS gradient over the progress bar's background color.
+		/// </summary>
+		[Parameter] public bool Striped { get; set; }
+
+		/// <summary>
+		/// If <c>true</c>, stripes are animated right to left via CSS3 animations. Has to be combined with <c>Striped</c>.
+		/// </summary>
+		[Parameter] public bool Animated { get; set; }
+
+		private string GetStripedCssClass()
+		{
+			string classes = "";
+
+			if (Striped)
+			{
+				classes = "progress-bar-striped";
+
+				if (Animated)
+				{
+					classes = "progress-bar-striped progress-bar-animated";
+				}
+			}
+
+			return classes;
+		}
+
+		private string GetColorCssClass()
+		{
+			return Color switch
+			{
+				null => null,
+				ThemeColor.None => null,
+				_ => "bg-" + this.Color.Value.ToString("f").ToLower()
+			};
+		}
+	}
+}
