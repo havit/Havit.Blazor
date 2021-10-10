68b98cb5ae7daf6f6c11811ede6dbe2c6f1b8650
Robert Haken <haken@havit.cz>
2021-10-10 22:03:45 +0200

    HxChipList - doc adjustments

--- Havit.Blazor.Components.Web.Bootstrap/Chips/HxChipList.razor.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Chips/HxChipList.razor.cs
@@ -4,10 +4,20 @@ using Microsoft.AspNetCore.Components;
 
 namespace Havit.Blazor.Components.Web.Bootstrap
 {
+	/// <summary>
+	/// Presents a list of chips as badges.<br/>
+	/// Usualy being used to present filter-criteria gathered by <see cref="HxFilterForm{TModel}"/>.
+	/// </summary>
 	public partial class HxChipList
 	{
+		/// <summary>
+		/// Chips to be presented.
+		/// </summary>
 		[Parameter] public IEnumerable<ChipItem> Chips { get; set; }
 
+		/// <summary>
+		/// Called when chip remove button is clicked.
+		/// </summary>
 		[Parameter] public EventCallback<ChipItem> OnChipRemoveClick { get; set; }
 
 		public async Task HandleRemoveClick(ChipItem chipItemToRemove)
