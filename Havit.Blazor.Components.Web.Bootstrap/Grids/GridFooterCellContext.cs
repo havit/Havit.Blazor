171d89ca60456897d3e00ac6fcc4d00c71c94657
Kanda, Jiří <kanda@havit.cz>
2021-11-01 09:56:01 +0100

    Kosmetika.

--- Havit.Blazor.Components.Web.Bootstrap/Grids/GridFooterCellContext.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Grids/GridFooterCellContext.cs
@@ -3,11 +3,11 @@
 	/// <summary>
 	/// Grid footer cell context.
 	/// </summary>
-	public class GridFooterCellContext
+	public record GridFooterCellContext
 	{
 		/// <summary>
 		/// Total count of items in the grid (includes all pages).
 		/// </summary>
-		public int? TotalCount { get; set; }
+		public int? TotalCount { get; init; }
 	}
 }
