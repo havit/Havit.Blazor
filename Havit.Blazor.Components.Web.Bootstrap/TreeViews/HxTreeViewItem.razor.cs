3e4f924b1abc7bd26c54578dd3627ae38db83590
Robert Haken <haken@havit.cz>
2021-11-04 02:09:22 +0100

    HxTreeView - coding standards (.editorconfig)

--- Havit.Blazor.Components.Web.Bootstrap/TreeViews/HxTreeViewItem.razor.cs
+++ Havit.Blazor.Components.Web.Bootstrap/TreeViews/HxTreeViewItem.razor.cs
@@ -4,25 +4,27 @@ using Microsoft.AspNetCore.Components;
 
 namespace Havit.Blazor.Components.Web.Bootstrap.TreeViews
 {
-    public partial class HxTreeViewItem<TValue>: ComponentBase
-    {
-        [Parameter]public ValueWrapper<TValue> Value { get; set; }
-        
-        private string PaddingLeft { get; set; }
+	public partial class HxTreeViewItem<TValue> : ComponentBase
+	{
+		[Parameter] public ValueWrapper<TValue> Value { get; set; }
 
-        protected override void OnInitialized()
-        {
-	        if(Value == null)
-		        return;
-	        
-            PaddingLeft = $"{Value.Level * 15}px";
-            Value.PropertyChanged += (_, _) => StateHasChanged();
-        }
+		private string PaddingLeft { get; set; }
 
-        private void OnItemClicked()
-        {
-	        Value.IsExpanded = !Value.IsExpanded;
-            Value.IsSelected = true;
-        }
-    }
+		protected override void OnInitialized()
+		{
+			if (Value == null)
+			{
+				return;
+			}
+
+			PaddingLeft = $"{Value.Level * 15}px";
+			Value.PropertyChanged += (_, _) => StateHasChanged();
+		}
+
+		private void OnItemClicked()
+		{
+			Value.IsExpanded = !Value.IsExpanded;
+			Value.IsSelected = true;
+		}
+	}
 }
