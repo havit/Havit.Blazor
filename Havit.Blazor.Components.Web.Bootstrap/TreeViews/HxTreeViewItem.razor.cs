d1c33206f9c365cf53984979dd0dc5fe6126fc3e
Dominik Crha <crha@havit.cz>
2021-11-15 16:14:32 +0100

    Frontend code-review

--- Havit.Blazor.Components.Web.Bootstrap/TreeViews/HxTreeViewItem.razor.cs
+++ Havit.Blazor.Components.Web.Bootstrap/TreeViews/HxTreeViewItem.razor.cs
@@ -8,19 +8,6 @@ namespace Havit.Blazor.Components.Web.Bootstrap
 	{
 		[Parameter] public ValueWrapper<TValue> Value { get; set; }
 
-		private string PaddingLeft { get; set; }
-
-		protected override void OnInitialized()
-		{
-			if (Value == null)
-			{
-				return;
-			}
-
-			PaddingLeft = $"{Value.Level * 15}px";
-			Value.PropertyChanged += (_, _) => StateHasChanged();
-		}
-
 		private void OnItemClicked()
 		{
 			Value.IsExpanded = !Value.IsExpanded;
