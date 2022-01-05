0887131fc205ddb0696784760a1bbcd078c4ff80
Robert Haken <haken@havit.cz>
2022-01-05 12:02:12 +0100

    HxListLayout - Defaults and Settings - CardSettings, FilterSubmitButtonSettings, FilterOpenButtonSettings and FilterOffcanvasSettings

--- Havit.Blazor.Components.Web.Bootstrap/Layouts/HxListLayout.nongeneric.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Layouts/HxListLayout.nongeneric.cs
@@ -6,8 +6,34 @@ using System.Threading.Tasks;
 
 namespace Havit.Blazor.Components.Web.Bootstrap
 {
+	/// <summary>
+	/// Non-generic API for <see cref="HxListLayout{TFilterModel}" />.
+	/// </summary>
 	public class HxListLayout
 	{
+		/// <summary>
+		/// Application-wide defaults for <see cref="HxListLayout{TFilterModel}"/> and derived components.
+		/// </summary>
+		public static ListLayoutSettings Defaults { get; set; }
+
+		static HxListLayout()
+		{
+			Defaults = new ListLayoutSettings()
+			{
+				CardSettings = new CardSettings(),
+				FilterSubmitButtonSettings = new ButtonSettings()
+				{
+					Color = ThemeColor.Primary,
+				},
+				FilterOpenButtonSettings = new ButtonSettings()
+				{
+					Icon = BootstrapIcon.Filter,
+					Color = ThemeColor.Light,
+				},
+				FilterOffcanvasSettings = new OffcanvasSettings(),
+			};
+		}
+
 		/// <summary>
 		/// Can be used for TFilterModelType to express there is no filter in the <see cref="HxListLayout"/> component.
 		/// </summary>
--- Havit.Blazor.Components.Web.Bootstrap/Layouts/HxListLayout.razor.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Layouts/HxListLayout.razor.cs
@@ -15,6 +15,12 @@ namespace Havit.Blazor.Components.Web.Bootstrap
 	/// <typeparam name="TFilterModel"></typeparam>
 	public partial class HxListLayout<TFilterModel>
 	{
+		/// <summary>
+		/// Returns <see cref="HxMessageBox"/> defaults.
+		/// Enables overriding defaults in descandants (use separate set of defaults).
+		/// </summary>
+		protected virtual ListLayoutSettings GetDefaults() => HxListLayout.Defaults;
+
 		[Parameter] public string Title { get; set; }
 
 		[Parameter] public RenderFragment TitleTemplate { get; set; }
@@ -38,12 +44,39 @@ namespace Havit.Blazor.Components.Web.Bootstrap
 
 		[Parameter] public RenderFragment CommandsTemplate { get; set; }
 
+		/// <summary>
+		/// Settings for the wrapping <see cref="HxCard"/>.
+		/// </summary>
+		[Parameter] public CardSettings CardSettings { get; set; }
+		protected CardSettings CardSettingsEffective => this.CardSettings ?? GetDefaults().CardSettings ?? throw new InvalidOperationException(nameof(CardSettings) + " default for " + nameof(HxListLayout) + " has to be set.");
+
+		/// <summary>
+		/// Settings for the <see cref="HxButton"/> opening filtering offcanvas.
+		/// </summary>
+		[Parameter] public ButtonSettings FilterOpenButtonSettings { get; set; }
+		protected ButtonSettings FilterOpenButtonSettingsEffective => this.FilterOpenButtonSettings ?? GetDefaults().FilterOpenButtonSettings ?? throw new InvalidOperationException(nameof(FilterOpenButtonSettings) + " default for " + nameof(HxListLayout) + " has to be set.");
+
+		/// <summary>
+		/// Settings for the <see cref="HxButton"/> submitting the filter.
+		/// </summary>
+		[Parameter] public ButtonSettings FilterSubmitButtonSettings { get; set; }
+		protected ButtonSettings FilterSubmitButtonSettingsEffective => this.FilterSubmitButtonSettings ?? GetDefaults().FilterSubmitButtonSettings ?? throw new InvalidOperationException(nameof(FilterSubmitButtonSettings) + " default for " + nameof(HxListLayout) + " has to be set.");
+
+		/// <summary>
+		/// Settings for the <see cref="HxOffcanvas"/> with the filter.
+		/// </summary>
+		[Parameter] public OffcanvasSettings FilterOffcanvasSettings { get; set; }
+		protected OffcanvasSettings FilterOffcanvasSettingsEffective => this.FilterOffcanvasSettings ?? GetDefaults().FilterOffcanvasSettings ?? throw new InvalidOperationException(nameof(FilterOffcanvasSettings) + " default for " + nameof(HxListLayout) + " has to be set.");
+
+
 		[Inject] protected IStringLocalizer<HxListLayout> Localizer { get; set; }
+
 
 		private ChipItem[] chips;
 		private string filterFormId = "hx" + Guid.NewGuid().ToString("N");
 		private HxFilterForm<TFilterModel> filterForm;
 		private HxOffcanvas filterOffcanvasComponent;
+
 
 		protected override void OnParametersSet()
 		{
