a23f402ff61eb7d7224550e7c16d21be7bf13d67
Robert Haken <haken@havit.cz>
2022-01-23 16:43:50 +0100

    #71 BlazorAppTest - HxInputTextTest

---
+++ BlazorAppTest/Pages/MyInputText.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Havit.Blazor.Components.Web.Bootstrap;
+
+namespace BlazorAppTest.Pages
+{
+	public class MyInputText : HxInputText
+	{
+		protected override string FormatValueAsString(string value)
+		{
+			if (value == "24h")
+			{
+				return "24:00:00";
+			}
+			return value;
+		}
+	}
+}
