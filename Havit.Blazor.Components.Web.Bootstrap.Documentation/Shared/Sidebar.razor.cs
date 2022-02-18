566c7903a0119d0be020d69d54a3bc71003bccca
Mikuláš Hoblík <mikuhoblik@gmail.com>
2022-02-18 16:52:43 +0100

    Themes - Bootstrap 5 theme

--- Havit.Blazor.Components.Web.Bootstrap.Documentation/Shared/Sidebar.razor.cs
+++ Havit.Blazor.Components.Web.Bootstrap.Documentation/Shared/Sidebar.razor.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -9,16 +10,15 @@ public partial class Sidebar
 {
 	private static readonly HttpClient client = new HttpClient();
 
-	private List<Theme> themes = new()
-	{
-		new() { Name = "Bootstrap 5", CssCdn = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" }
-	};
+	private List<Theme> themes = new();
 	private Theme selectedTheme;
 
 	protected override async Task OnInitializedAsync()
 	{
 		var result = await client.GetStringAsync("https://bootswatch.com/api/5.json");
 		themes = JsonConvert.DeserializeObject<ThemeHolder>(result).Themes;
+
+		themes = themes.Prepend(new() { Name = "Bootstrap 5", CssCdn = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" }).ToList();
 	}
 }
 
