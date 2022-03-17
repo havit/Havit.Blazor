6fac55969bc708e2ef26b48689584a5d5a04665d
Robert Haken <haken@havit.cz>
2022-03-17 01:43:00 +0100

    HxChipList - ChipsBadgeSettings, Defaults, Settings, CssClass

---
+++ Havit.Blazor.Components.Web.Bootstrap/Chips/ChipListSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace Havit.Blazor.Components.Web.Bootstrap;
+
+public class ChipListSettings
+{
+	/// <summary>
+	/// Settings for the <see cref="HxBadge"/> used to render chips.
+	/// </summary>
+	public BadgeSettings ChipBadgeSettings { get; set; }
+
+	/// <summary>
+	/// Additional CSS class.
+	/// </summary>
+	public string CssClass { get; set; }
+}
--- Havit.Blazor.Components.Web.Bootstrap/Chips/HxChipList.razor.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Chips/HxChipList.razor.cs
@@ -7,9 +7,57 @@
 	public partial class HxChipList
 	{
 		/// <summary>
+		/// Application-wide defaults for the <see cref="HxChipList"/>.
+		/// </summary>
+		public static ChipListSettings Defaults { get; set; }
+
+		static HxChipList()
+		{
+			Defaults = new ChipListSettings()
+			{
+				ChipBadgeSettings = new BadgeSettings()
+				{
+					Color = ThemeColor.Secondary,
+				}
+			};
+		}
+
+		/// <summary>
+		/// Returns application-wide defaults for the component.
+		/// Enables overriding defaults in descandants (use separate set of defaults).
+		/// </summary>
+		protected virtual ChipListSettings GetDefaults() => Defaults;
+
+		/// <summary>
+		/// Set of settings to be applied to the component instance (overrides <see cref="Defaults"/>, overriden by individual parameters).
+		/// </summary>
+		[Parameter] public ChipListSettings Settings { get; set; }
+
+		/// <summary>
+		/// Returns optional set of component settings.
+		/// </summary>
+		/// <remarks>
+		/// Simmilar to <see cref="GetDefaults"/>, enables defining wider <see cref="Settings"/> in components descandants (by returning a derived settings class).
+		/// </remarks>
+		protected virtual ChipListSettings GetSettings() => this.Settings;
+
+		/// <summary>
 		/// Chips to be presented.
 		/// </summary>
 		[Parameter] public IEnumerable<ChipItem> Chips { get; set; }
+
+		/// <summary>
+		/// Settings for the <see cref="HxBadge"/> used to render chips.
+		/// Default brings <c>Color="<see cref="ThemeColor.Secondary" />".</c>
+		/// </summary>
+		[Parameter] public BadgeSettings ChipBadgeSettings { get; set; }
+		protected BadgeSettings ChipBadgeSettingsEffective => this.ChipBadgeSettings ?? this.GetSettings()?.ChipBadgeSettings ?? this.GetDefaults().ChipBadgeSettings ?? throw new InvalidOperationException(nameof(ChipBadgeSettings) + " default for " + nameof(HxChipList) + " has to be set.");
+
+		/// <summary>
+		/// Additional CSS class.
+		/// </summary>
+		[Parameter] public string CssClass { get; set; }
+		protected string CssClassEffective => this.CssClass ?? this.GetSettings()?.CssClass ?? GetDefaults().CssClass;
 
 		/// <summary>
 		/// Called when chip remove button is clicked.
