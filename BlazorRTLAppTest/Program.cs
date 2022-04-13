7b0bce718deb4fbb57a111df466bb280131f72ce
Mikuláš Hoblík <mikuhoblik@gmail.com>
2022-04-13 18:39:48 +0200

    #61178 Připravit stránku pro testování RTL podpory Havit.Blazor

---
+++ BlazorRTLAppTest/Program.cs
@@ -0,0 +1,36 @@
+using BlazorRTLAppTest.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Havit.Blazor.Components.Web;
+using Havit.Blazor.Components.Web.Bootstrap;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddHxServices();
+builder.Services.AddHxMessageBoxHost();
+builder.Services.AddHxMessenger();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
