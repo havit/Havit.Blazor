661a2a291f45780b8156593638827dc3f0110919
Robert Haken <haken@havit.cz>
2022-05-13 14:18:05 +0200

    AssemblyInfo.cs stylecop

--- Havit.Blazor.Components.Web.Bootstrap/Properties/AssemblyInfo.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@
 // To understand this well see comment in ResourceManagerStringLocalizerFactory.GetResourcePath (https://github.com/aspnet/Localization/blob/c283dfb56cd9620edfae644f1229d8231e9cfc0e/src/Microsoft.Extensions.Localization/ResourceManagerStringLocalizerFactory.cs#L249).
 // We would like to share resources near classes, unfortunatelly we cannot use ResourceLocationAttribute with an empty string (https://github.com/aspnet/Localization/blob/43b974482c7b703c92085c6f68b3b23d8fe32720/src/Microsoft.Extensions.Localization/ResourceLocationAttribute.cs#L22).
 // Thus we are forced to use some namespace.
-[assembly: Microsoft.Extensions.Localization.ResourceLocation("Resources")] 
+[assembly: Microsoft.Extensions.Localization.ResourceLocation("Resources")]
