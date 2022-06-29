2cb87329977c625bd159c4d5f37d5a21ac675d3b
Mikuláš Hoblík <mikuhoblik@gmail.com>
2022-06-29 11:16:27 +0200

    [HxSidebar] Custom toggler template parameter - adj #211

--- Havit.Blazor.Components.Web.Bootstrap/Navigation/SidebarTogglerTemplateContext.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Navigation/SidebarTogglerTemplateContext.cs
@@ -6,10 +6,13 @@ using System.Threading.Tasks;
 
 namespace Havit.Blazor.Components.Web.Bootstrap;
 
+/// <summary>
+/// Context provided to the <see cref="HxSidebar.TogglerTemplate" />.
+/// </summary>
 public class SidebarTogglerTemplateContext
 {
 	/// <summary>
 	/// Indicates whether a <see cref="HxSidebar"/> is collapsed or expanded.
 	/// </summary>
-	[Parameter] public bool Collapsed { get; set; }
+	public bool Collapsed { get; set; }
 }
