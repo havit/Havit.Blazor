94ae099b7a8e65293553bedf42f956c2f0ee043a
Mikuláš Hoblík <mikuhoblik@gmail.com>
2022-07-26 16:42:25 +0200

    [HxSidebarBrand] LogoShort for different image in collapsed state #264

--- Havit.Blazor.Components.Web.Bootstrap/Navigation/HxSidebarBrand.razor.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Navigation/HxSidebarBrand.razor.cs
@@ -16,8 +16,28 @@
 		[Parameter] public RenderFragment Logo { get; set; }
 
 		/// <summary>
+		/// Brand logo to be displayed when the <see cref="HxSidebar"/> is collapsed.
+		/// </summary>
+		[Parameter] public RenderFragment LogoShort { get; set; }
+
+		/// <summary>
 		/// Brand short name.
 		/// </summary>
 		[Parameter] public string BrandNameShort { get; set; }
+
+		/// <summary>
+		/// Adjusts the css class for the element to be hidden when the containg sidebar is collapsed.
+		/// </summary>
+		/// <param name="cssClass"></param>
+		/// <returns></returns>
+		private string GetCssClass(string cssClass)
+		{
+			if (LogoShort is not null)
+			{
+				cssClass += " hx-hide-when-in-collapsed-sidebar";
+			}
+
+			return cssClass;
+		}
 	}
 }
