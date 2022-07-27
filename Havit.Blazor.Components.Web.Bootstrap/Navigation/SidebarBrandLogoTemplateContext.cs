2a5b925352a528662173a00c6a182886bdbe9172
Mikuláš Hoblík <mikuhoblik@gmail.com>
2022-07-27 15:43:50 +0200

    [HxSidebar] change contexts' property names, adjustments #264

--- Havit.Blazor.Components.Web.Bootstrap/Navigation/SidebarBrandLogoTemplateContext.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Navigation/SidebarBrandLogoTemplateContext.cs
@@ -9,7 +9,10 @@ namespace Havit.Blazor.Components.Web.Bootstrap;
 /// <summary>
 /// Context provided to the <see cref="HxSidebarBrand.LogoTemplate" />.
 /// </summary>
-public class SidebarBrandLogoTemplateContext : SidebarCollapsedContext
+public class SidebarBrandLogoTemplateContext
 {
-
+	/// <summary>
+	/// Indicates whether a <see cref="HxSidebar"/> is collapsed or expanded.
+	/// </summary>
+	public bool SidebarCollapsed { get; set; }
 }
--- Havit.Blazor.Components.Web.Bootstrap/Navigation/SidebarTogglerTemplateContext.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Navigation/SidebarTogglerTemplateContext.cs
@@ -9,7 +9,10 @@ namespace Havit.Blazor.Components.Web.Bootstrap;
 /// <summary>
 /// Context provided to the <see cref="HxSidebar.TogglerTemplate" />.
 /// </summary>
-public class SidebarTogglerTemplateContext : SidebarCollapsedContext
+public class SidebarTogglerTemplateContext
 {
-
+	/// <summary>
+	/// Indicates whether a <see cref="HxSidebar"/> is collapsed or expanded.
+	/// </summary>
+	public bool SidebarCollapsed { get; set; }
 }
