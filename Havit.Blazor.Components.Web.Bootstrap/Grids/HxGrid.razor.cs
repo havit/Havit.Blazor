8ad58fc913ce1e7cf5124fa81f749085a5810df1
Jiří Kanda <kanda@havit.cz>
2022-08-24 17:28:34 +0200

    [HxGrid] InfiniteScroll + synchronous DataProvider returning empty data => DOMException: Failed to execute 'setStartAfter' on 'Range': the given Node has no parent. #303

--- Havit.Blazor.Components.Web.Bootstrap/Grids/HxGrid.razor.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Grids/HxGrid.razor.cs
@@ -566,8 +566,8 @@ namespace Havit.Blazor.Components.Web.Bootstrap
 				CancellationToken = request.CancellationToken
 			};
 
-			await Task.Yield(); // fixed issue 303 (https://github.com/havit/Havit.Blazor/issues/303)
 			GridDataProviderResult<TItem> gridDataProviderResponse = await InvokeDataProviderInternal(gridDataProviderRequest);
+			await Task.Yield(); // fixed issue 303 (https://github.com/havit/Havit.Blazor/issues/303)
 
 			if (!request.CancellationToken.IsCancellationRequested)
 			{
