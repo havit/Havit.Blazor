253999e26e4a51515953b417cec80059faef2909
Jiří Kanda <kanda@havit.cz>
2022-08-26 14:49:07 +0200

    [HxCheckbox] Replacement of [HxInputCheckbox] with Text instead of Label #191 - support for Inline

--- Havit.Blazor.Components.Web.Bootstrap/Forms/HxCheckbox.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Forms/HxCheckbox.cs
@@ -22,17 +22,37 @@ namespace Havit.Blazor.Components.Web.Bootstrap
 		/// </summary>
 		[Parameter] public RenderFragment TextTemplate { get; set; }
 
+		/// <summary>
+		/// Allows grouping checkboxes on the same horizontal row by rendering them inline. Default is <c>false</c>.		
+		/// Works only when there is no label, no hint and no validation message.
+		/// </summary>
+		[Parameter] public bool Inline { get; set; }
 
 		[Inject] protected IStringLocalizer<HxCheckbox> Localizer { get; set; }
 
 		/// <inheritdoc cref="HxInputBase{TValue}.CoreInputCssClass" />
 		private protected override string CoreInputCssClass => "form-check-input";
 
+		/// <inheritdoc cref="HxInputBase{TValue}.CoreCssClass" />		
+		private protected override string CoreCssClass => Inline ? CssClassHelper.Combine(base.CoreCssClass, "form-check form-check-inline") : base.CoreCssClass;
+
 		/// <inheritdoc />
 		protected override void BuildRenderInput(RenderTreeBuilder builder)
 		{
-			builder.OpenElement(-2, "div");
-			builder.AddAttribute(-1, "class", "form-check");
+			// for Inline chheckbox, we add form-check, form-check-inline to the parent div (allows combining with CssClass etc.).
+			// It is expected there is just the parent div, input and label for Text/TextTemplate.
+			// No label for Label/LabelTemplate, no HintTemplate, no validation message is expected.
+
+			// For regular checkbox, there is a parent div followed by label for Label/LabelTemplate.
+			// Siblings label, input, label does not work well and there is no where to add form-check.
+			// There for we wrap the input and label to the additional div.
+			if (!Inline)
+			{
+				builder.OpenElement(-2, "div");
+				builder.AddAttribute(-1, "class", "form-check");
+			}
+
+			EnsureInputId(); // must be called before the input is rendered
 
 			builder.OpenElement(0, "input");
 			BuildRenderInput_AddCommonAttributes(builder, "checkbox");
@@ -52,7 +72,10 @@ namespace Havit.Blazor.Components.Web.Bootstrap
 			builder.AddContent(2004, TextTemplate);
 			builder.CloseElement(); // label
 
-			builder.CloseElement(); // div
+			if (!Inline)
+			{
+				builder.CloseElement(); // div
+			}
 		}
 
 		/// <inheritdoc />
