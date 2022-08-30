5732c29d4f13fe1f42eda8b7d11e40ccc7516d04
Robert Haken <haken@havit.cz>
2022-08-31 01:40:06 +0200

    HxCheckbox, HxCheckboxList, HxSwitch, demos, obsolete #332 #191

---
+++ Havit.Blazor.Components.Web.Bootstrap/Forms/HxSwitch.cs
@@ -0,0 +1,12 @@
+﻿namespace Havit.Blazor.Components.Web.Bootstrap;
+
+/// <summary>
+/// Switch input.<br/>
+/// (Replaces the former <see cref="HxInputSwitch"/> component which is now obsolete.)<br/>
+/// Full documentation and demos: <see href="https://havit.blazor.eu/components/HxSwitch">https://havit.blazor.eu/components/HxSwitch</see>
+/// </summary>
+public class HxSwitch : HxCheckbox
+{
+	/// <inheritdoc cref="HxCheckbox.CoreFormElementCssClass" />
+	private protected override string CoreFormElementCssClass => "form-check form-switch";
+}
