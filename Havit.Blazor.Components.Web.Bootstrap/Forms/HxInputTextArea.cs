6ff02c3abdef7af8b55e4a78888f36bd3f22682e
Mikuláš Hoblík <mikuhoblik@gmail.com>
2022-08-30 11:00:08 +0200

    doc - HxInputTextArea don't display link to documentation page on the documentation page

--- Havit.Blazor.Components.Web.Bootstrap/Forms/HxInputTextArea.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Forms/HxInputTextArea.cs
@@ -3,7 +3,7 @@
 	/// <summary>
 	/// <see href="https://getbootstrap.com/docs/5.0/forms/floating-labels/#textareas" target="_blank">Textarea</see>.
 	/// To set a custom height, do not use the rows attribute. Instead, set an explicit height (either inline or via custom CSS).<br />
-	/// Full documentation and demos: <see href="https://havit.blazor.eu/components/HxInputTextArea">https://havit.blazor.eu/components/HxInputTextArea</see>.
+	/// Full documentation and demos: <see href="https://havit.blazor.eu/components/HxInputTextArea">https://havit.blazor.eu/components/HxInputTextArea</see>
 	/// </summary>
 	public class HxInputTextArea : HxInputText
 	{
