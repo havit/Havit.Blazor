a58f84851ae1cee9ccb4d644cedb9804b8c95c60
Robert Haken <haken@havit.cz>
2024-01-11 10:50:45 +0100

    OperatingSystem.IsBrowser() simplification

--- Havit.Blazor.Components.Web/Messenger/MessengerServiceCollectionExtensions.cs
+++ Havit.Blazor.Components.Web/Messenger/MessengerServiceCollectionExtensions.cs
@@ -13,7 +13,7 @@ public static class MessengerServiceCollectionExtensions
 	/// </summary>
 	public static IServiceCollection AddHxMessenger(this IServiceCollection services)
 	{
-		if (RuntimeInformation.IsOSPlatform(OSPlatform.Create("BROWSER")))
+		if (OperatingSystem.IsBrowser())
 		{
 			// Allows gRPC Interceptors and HttpMessageHandlers to pass error messages to the HxMessenger without having to struggle with different DI Scope
 			return services.AddSingleton<IHxMessengerService, HxMessengerService>();
