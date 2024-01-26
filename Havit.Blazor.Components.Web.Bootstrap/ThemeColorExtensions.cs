e27878047b508d5a7e76a5c84f31bdfb648042ea
dominikcrha <crha@havit.cz>
2024-01-26 11:06:37 +0100

    Added ToBorderColorCss + emphasis/subtle variants

--- Havit.Blazor.Components.Web.Bootstrap/ThemeColorExtensions.cs
+++ Havit.Blazor.Components.Web.Bootstrap/ThemeColorExtensions.cs
@@ -2,23 +2,32 @@
 
 public static class ThemeColorExtensions
 {
-	public static string ToBackgroundColorCss(this ThemeColor themeColor)
+	public static string ToBackgroundColorCss(this ThemeColor themeColor, bool subtle = false)
 	{
 		return themeColor switch
 		{
 			ThemeColor.None => null,
 			ThemeColor.Link => throw new NotSupportedException($"{nameof(ThemeColor)}.{nameof(ThemeColor.Link)} cannot be used as background color."),
-			_ => "bg-" + themeColor.ToString("f").ToLower()
+			_ => "bg-" + themeColor.ToString("f").ToLower() + (subtle ? "-subtle" : null)
 		};
 	}
 
-	public static string ToTextColorCss(this ThemeColor themeColor)
+	public static string ToTextColorCss(this ThemeColor themeColor, bool emphasis = false)
 	{
 		return themeColor switch
 		{
 			ThemeColor.None => null,
 			ThemeColor.Link => throw new NotSupportedException($"{nameof(ThemeColor)}.{nameof(ThemeColor.Link)} cannot be used as text color."),
-			_ => "text-" + themeColor.ToString("f").ToLower()
+			_ => "text-" + themeColor.ToString("f").ToLower() + (emphasis ? "-emphasis" : null)
+		};
+	}
+	public static string ToBorderColorCss(this ThemeColor themeColor, bool subtle = false)
+	{
+		return themeColor switch
+		{
+			ThemeColor.None => null,
+			ThemeColor.Link => throw new NotSupportedException($"{nameof(ThemeColor)}.{nameof(ThemeColor.Link)} cannot be used as border color."),
+			_ => "border-" + themeColor.ToString("f").ToLower() + (subtle ? "-subtle" : null)
 		};
 	}
 }
