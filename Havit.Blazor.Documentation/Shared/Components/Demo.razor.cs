272f722d7a0b33155e177657fe7cb546c100bab5
Robert Haken <haken@havit.cz>
2024-06-11 01:42:30 +0200

    doc - Demo.razor refactoring

--- Havit.Blazor.Documentation/Shared/Components/Demo.razor.cs
+++ Havit.Blazor.Documentation/Shared/Components/Demo.razor.cs
@@ -6,15 +6,25 @@ namespace Havit.Blazor.Documentation.Shared.Components;
 public partial class Demo : PerformanceLoggingComponentBase
 {
 	[Parameter] public Type Type { get; set; }
-
 	[Parameter] public bool Tabs { get; set; } = true;
-
 	[Parameter] public string DemoCardCssClass { get; set; }
 
 	[Inject] protected IJSRuntime JSRuntime { get; set; }
 
-	private bool _showingDemo = true;
 	private string _code;
+	private readonly RenderFragment _renderDemo;
+	private readonly RenderFragment _renderCode;
+	private readonly CardSettings _cardSettings = new()
+	{
+		CssClass = "card-demo my-3",
+		BodyCssClass = "p-0"
+	};
+
+	public Demo()
+	{
+		_renderDemo = RenderDemo;
+		_renderCode = RenderCode;
+	}
 
 	protected override async Task OnParametersSetAsync()
 	{
@@ -43,17 +53,5 @@ public partial class Demo : PerformanceLoggingComponentBase
 	protected override async Task OnAfterRenderAsync(bool firstRender)
 	{
 		await JSRuntime.InvokeVoidAsync("highlightCode");
-	}
-
-	private Task HandleDemoTabActivated()
-	{
-		_showingDemo = true;
-		return Task.CompletedTask;
-	}
-
-	private async Task HandleCodeTabActivated()
-	{
-		_showingDemo = false;
-		await LoadCodeAsync();
 	}
 }
