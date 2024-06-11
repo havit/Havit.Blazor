63584a582e181dfd6245f34fd69d654b238f32aa
Robert Haken <haken@havit.cz>
2024-06-11 10:15:21 +0200

    #822 HxTabPanel - refactoring (tabs registration)

---
+++ Havit.Blazor.Components.Web/Internal/HxDeferInternal.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace Havit.Blazor.Components.Web.Internal;
+
+// This is used by components to move its body rendering to the end of the render queue so we can collect
+// the list of child components first. It has to be public only because it's used from .razor logic.
+
+/// <summary>
+/// For internal use only. Do not use.
+/// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
+public sealed class HxDeferInternal : ComponentBase
+{
+	/// <summary>
+	/// For internal use only. Do not use.
+	/// </summary>
+	[Parameter] public RenderFragment ChildContent { get; set; }
+
+	/// <summary>
+	/// For internal use only. Do not use.
+	/// </summary>
+	protected override void BuildRenderTree(RenderTreeBuilder builder)
+	{
+		builder.AddContent(0, ChildContent);
+	}
+}
+
