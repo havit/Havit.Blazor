b3883c90f5cb8f159c0592cea1e6d8b9b579afd3
Mikuláš Hoblík <mikuhoblik@gmail.com>
2024-10-31 10:00:09 +0100

    Gateway page to HBP GitHub repo

---
+++ Havit.Blazor.Documentation/Pages/Premium/GatewayToGithub.razor.cs
@@ -0,0 +1,25 @@
+﻿namespace Havit.Blazor.Documentation.Pages.Premium;
+
+public partial class GatewayToGithub
+{
+	[SupplyParameterFromQuery] public string Url { get; set; }
+	[Inject] private NavigationManager NavigationManager { get; set; }
+
+	private bool _skipGatewayPage = false;
+
+	private void LearnMoreAboutPremium()
+	{
+		NavigationManager.NavigateTo("/premium");
+	}
+
+	private void ContinueToGitHub()
+	{
+		if (_skipGatewayPage)
+		{
+			// TODO: save preference into a cookie and redirect user right away if the cookie is present
+		}
+
+		var unescaped = Uri.UnescapeDataString(Url);
+		NavigationManager.NavigateTo(unescaped);
+	}
+}
