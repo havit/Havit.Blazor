800483107a3f53ffb1649b9b08cb6ccffe3581c8
Mikuláš Hoblík <mikuhoblik@gmail.com>
2024-10-31 11:02:22 +0100

    Gateway page to HBP GitHub repo - skip next time functionality

--- Havit.Blazor.Documentation/Pages/Premium/GatewayToGithub.razor.cs
+++ Havit.Blazor.Documentation/Pages/Premium/GatewayToGithub.razor.cs
@@ -1,25 +1,50 @@
-﻿namespace Havit.Blazor.Documentation.Pages.Premium;
+﻿using Microsoft.JSInterop;
 
-public partial class GatewayToGithub
+namespace Havit.Blazor.Documentation.Pages.Premium;
+
+public partial class GatewayToGitHub
 {
 	[SupplyParameterFromQuery] public string Url { get; set; }
+
 	[Inject] private NavigationManager NavigationManager { get; set; }
+	[Inject] private IJSRuntime JSRuntime { get; set; }
+
+	private IJSObjectReference _jsModule;
 
 	private bool _skipGatewayPage = false;
+	private string _gitHubFolderUrl = "https://github.com/havit/Havit.Blazor.Premium";
+
+	protected override void OnParametersSet()
+	{
+		_gitHubFolderUrl = Uri.UnescapeDataString(Url);
+	}
 
 	private void LearnMoreAboutPremium()
 	{
 		NavigationManager.NavigateTo("/premium");
 	}
 
-	private void ContinueToGitHub()
+	private async Task ContinueToGitHub()
 	{
 		if (_skipGatewayPage)
 		{
-			// TODO: save preference into a cookie and redirect user right away if the cookie is present
+			await EnsureJsModuleAsync();
+			await _jsModule.InvokeVoidAsync("setSkipGatewayPage", true);
 		}
+		NavigationManager.NavigateTo(_gitHubFolderUrl);
+	}
 
-		var unescaped = Uri.UnescapeDataString(Url);
-		NavigationManager.NavigateTo(unescaped);
+	private async Task EnsureJsModuleAsync()
+	{
+		_jsModule ??= await JSRuntime.ImportModuleAsync($"./Pages/Premium/{nameof(GatewayToGitHub)}.razor.js");
+	}
+
+	private MarkupString GenerateHeadContent()
+	{
+		return (MarkupString)$@"<script>
+					 if (document.cookie.split(';').some((item) => item.trim() === 'SkipGatewayPage=true')) {{
+					 	 window.location.href = '{_gitHubFolderUrl}'
+					 }}
+				  </script>";
 	}
 }
