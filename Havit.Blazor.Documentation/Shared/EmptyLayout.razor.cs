bda7ad2845b8b67c8cb3368d233da3ef7617ee88
Mikuláš Hoblík <62853076+Harvey1214@users.noreply.github.com>
2024-11-02 01:28:33 +0100

    [Doc] Canonical URLs metadata #914 (#915)
    * [Doc] Canonical URLs metadata #914
    
    * [Doc] Canonical URLs metadata #914
    
    * [Doc] Canonical URLs metadata #914 - PageCanonicalUrl + types and components
    
    * consolidation
    
    * Premium content
    
    ---------
    
    Co-authored-by: Robert Haken <haken@havit.cz>

---
+++ Havit.Blazor.Documentation/Shared/EmptyLayout.razor.cs
@@ -0,0 +1,15 @@
+﻿using Havit.Blazor.Documentation.Shared.Components;
+
+namespace Havit.Blazor.Documentation.Shared;
+
+public partial class EmptyLayout
+{
+	[Inject] protected NavigationManager NavigationManager { get; set; }
+
+	private PageCanonicalUrlTracker _pageCanonicalUrlTracker;
+
+	protected override void OnInitialized()
+	{
+		_pageCanonicalUrlTracker = new PageCanonicalUrlTracker(NavigationManager);
+	}
+}
