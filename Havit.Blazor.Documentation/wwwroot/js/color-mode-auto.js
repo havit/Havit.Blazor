8863c6708a9e11f2aae3ecc3c501f8ccd5fb307c
Robert Haken <haken@havit.cz>
2024-11-06 01:58:11 +0100

    [doc] JS optimization

--- Havit.Blazor.Documentation/wwwroot/js/color-mode-auto.js
+++
@@ -1,3 +0,0 @@
-﻿if ((document.documentElement.getAttribute('data-bs-theme') === 'auto') && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-	document.documentElement.setAttribute('data-bs-theme', 'dark');
-}
