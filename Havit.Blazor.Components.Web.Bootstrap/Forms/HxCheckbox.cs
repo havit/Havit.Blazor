e10a5ab64c5603fd1e585cece592b3b184267e16
Robert Haken <haken@havit.cz>
2024-11-12 13:21:09 +0100

    #886 [HxCheckbox] [HxSwitch] Use Text instead of Yes/No for chips

--- Havit.Blazor.Components.Web.Bootstrap/Forms/HxCheckbox.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Forms/HxCheckbox.cs
@@ -131,6 +131,16 @@ public class HxCheckbox : HxInputBase<bool>
 
 	protected override void RenderChipValue(RenderTreeBuilder builder)
 	{
-		builder.AddContent(0, CurrentValue ? Localizer["ChipValueTrue"] : Localizer["ChipValueFalse"]);
+		// #886 [HxCheckbox] [HxSwitch] Use Text instead of Yes/No for chips
+		// If both Text and Label are set, use Text for the positive chip value.
+		// BTW: Negative value is currently never used as chip is rendered only if the value is not equal to default(TValue).
+		// This might need additional attention if we implement support for three-state checkboxes
+		// or allow setting neutral value other than default(TValue).
+		string positiveValue = Localizer["ChipValueTrue"];
+		if (!String.IsNullOrWhiteSpace(Text) && !String.IsNullOrWhiteSpace(Label))
+		{
+			positiveValue = Text;
+		}
+		builder.AddContent(0, CurrentValue ? positiveValue : Localizer["ChipValueFalse"]);
 	}
 }
