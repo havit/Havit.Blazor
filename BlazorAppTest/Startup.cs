d0cfa113958092b4006d1acc339d9a2741d688f5
Robert Haken <haken@havit.cz>
2025-02-11 11:32:28 +0100

    StrongApiStringLocalizer - project references consolidation; test moved to new TestApp

--- BlazorAppTest/Startup.cs
+++ BlazorAppTest/Startup.cs
@@ -1,6 +1,4 @@
 using System.Globalization;
-using BlazorAppTest.Resources;
-using Havit.Blazor.Components.Web;
 using Havit.Blazor.Documentation.DemoData;
 using Havit.Blazor.GoogleTagManager;
 
@@ -22,7 +20,6 @@ public class Startup
 		services.AddLogging();
 
 		services.AddLocalization();
-		services.AddGeneratedResourceWrappers();
 
 		services.AddRazorPages();
 		services.AddServerSideBlazor();
--- Havit.Blazor.TestApp/Havit.Blazor.TestApp.Client/DependencyInjectionExtensions.cs
+++ Havit.Blazor.TestApp/Havit.Blazor.TestApp.Client/DependencyInjectionExtensions.cs
@@ -1,13 +1,14 @@
-﻿using Havit.Blazor.Components.Web;
-
-namespace Havit.Blazor.TestApp.Client;
+﻿namespace Havit.Blazor.TestApp.Client;
 
 public static class DependencyInjectionExtensions
 {
 	public static IServiceCollection AddClientServices(this IServiceCollection services)
 	{
 		services.AddHxServices();
+
 		services.AddTransient<IDemoDataService, DemoDataService>();
+		services.AddGeneratedResourceWrappers();
+
 
 		return services;
 	}
