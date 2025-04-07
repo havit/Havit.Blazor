63b95402f9d1122750e032dd95639b1e52079a7e
Robert Haken <haken@havit.cz>
2025-04-07 10:11:11 +0200

    closes #1056 [HxPlaceholderContainer] Replace existing HTML element

--- Havit.Blazor.Components.Web.Bootstrap/Placeholders/PlaceholderContainerSettings.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Placeholders/PlaceholderContainerSettings.cs
@@ -6,6 +6,11 @@
 public record PlaceholderContainerSettings
 {
 	/// <summary>
+	/// Gets or sets the name of the element to render.
+	/// </summary>
+	public string ElementName { get; set; }
+
+	/// <summary>
 	/// Animation of the placeholders in the container.
 	/// </summary>
 	public PlaceholderAnimation? Animation { get; set; }
