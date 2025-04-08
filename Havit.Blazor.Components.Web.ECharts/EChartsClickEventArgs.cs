3e46b6399e571f7e56dc39bbdbe08098f234ce29
Robert Haken <haken@havit.cz>
2025-04-08 05:43:00 -0700

    ECharts (#1060)
    * initial implementation
    
    * updated from BlocksApp
    
    * constructor injection
    
    * private JSFuncConverter
    
    * component docs
    
    * main documentation intro
    
    * JavaScript registration consolidation
    
    * basic demo
    
    * HeightUnit removal; OnClick improvements (stopPropagation, blank, targetType)
    
    * documentation + demos
    
    * demo - handling clicks
    
    * dark mode adjustments
    
    * OnClick demo adjustment - change data, show animation
    
    * OnClick demo cleanup

---
+++ Havit.Blazor.Components.Web.ECharts/EChartsClickEventArgs.cs
@@ -0,0 +1,29 @@
+﻿namespace Havit.Blazor.Components.Web.ECharts;
+
+public record EChartClickEventArgs
+{
+	/// <summary>
+	/// Gets or sets the index of the series.
+	/// </summary>
+	public int? SeriesIndex { get; set; }
+
+	/// <summary>
+	/// Gets or sets the index of the data point.
+	/// </summary>
+	public int? DataIndex { get; set; }
+
+	/// <summary>
+	/// Gets or sets the name of the data point.
+	/// </summary>
+	public string Name { get; set; }
+
+	/// <summary>
+	/// Gets or sets the value of the data point.
+	/// </summary>
+	public decimal? Value { get; set; }
+
+	/// <summary>
+	/// Indicates what type of click event is it.
+	/// </summary>
+	public string TargetType { get; set; }
+}
---
+++ Havit.Blazor.Components.Web.ECharts/wwwroot/HxEChart.js
@@ -0,0 +1,72 @@
+export function setupChart(id, dotnetReference, options, autoResize) {
+	let optionsObject = eval('(' + options + ')');
+
+	const element = document.getElementById(id);
+	if (!element) {
+		return;
+	}
+	if (!element.echart) {
+		const chart = echarts.init(element);
+		element.echartsDotnetReference = dotnetReference;
+
+		if (autoResize) {
+			element.resizeObserver = new ResizeObserver(() => { resizeChart(id); });
+			element.resizeObserver.observe(element);
+		}
+
+		chart.getZr().on('click', function (event) {
+			event.event.stopPropagation();
+			event.event.preventDefault();
+
+			if (!event.target) {
+				// blank click event
+				handleClick(dotnetReference);
+			}
+		});
+
+		chart.on('click', (params) => {
+			// echarts event
+			handleClick(dotnetReference, params);
+		});
+		element.echart = chart;
+	}
+
+	element.echart.setOption(optionsObject);
+}
+
+function handleClick(dotnetReference, params) {
+	dotnetReference.invokeMethodAsync('HandleClick', {
+		dataIndex: params?.dataIndex,
+		name: params?.name,
+		seriesIndex: params?.seriesIndex,
+		value: params?.value,
+		targetType: params?.targetType,
+	});
+}
+
+function resizeChart(id) {
+	const element = document.getElementById(id);
+	if (!element || !element.echart) {
+		return;
+	}
+
+	const chart = element.echart;
+	chart.resize();
+}
+
+export function dispose(id) {
+	const element = document.getElementById(id);
+	if (!element || !element.echart) {
+		return;
+	}
+
+	const chart = element.echart;
+
+	if (element.resizeObserver) {
+		element.resizeObserver.unobserve(element);
+		element.resizeObserver.disconnect();
+		element.resizeObserver = null;
+	}
+	chart.dispose();
+	element.echart = null;
+}
