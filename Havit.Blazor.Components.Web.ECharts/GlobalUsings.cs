f18ae0544e8d6f572ac5c07eb68349f84a1002b1
Robert Haken <haken@havit.cz>
2025-04-09 01:24:02 +0200

    Havit.Blazor.Components.Web.ECharts - unused dependency removal

--- Havit.Blazor.Components.Web.ECharts/GlobalUsings.cs
+++ Havit.Blazor.Components.Web.ECharts/GlobalUsings.cs
@@ -4,8 +4,6 @@ global using System.Linq;
 global using System.Text;
 global using System.Threading.Tasks;
 
-global using Havit.Diagnostics.Contracts;
-
 global using Microsoft.AspNetCore.Components;
 global using Microsoft.AspNetCore.Components.Rendering;
 global using Microsoft.AspNetCore.Components.Web;
