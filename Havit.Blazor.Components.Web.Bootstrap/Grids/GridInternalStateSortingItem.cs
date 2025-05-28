2f69165d86b0b3020f5c1a58f8cce21fa025f3c7
Jiří Kanda <kanda@havit.cz>
2025-05-28 14:13:59 +0200

    [HxGrid] Ability to reset grid state (position?) when refreshing data (e.g. after new filter applied) #415 (sorting)

--- Havit.Blazor.Components.Web.Bootstrap/Grids/GridInternalStateSortingItem.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Grids/GridInternalStateSortingItem.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Havit.Collections;
-
-namespace Havit.Blazor.Components.Web.Bootstrap;
+﻿namespace Havit.Blazor.Components.Web.Bootstrap;
 
 /// <summary>
 /// Grid internal state item to persist sorting.
 /// </summary>
-internal class GridInternalStateSortingItem<TItem>
+internal record GridInternalStateSortingItem<TItem> // record: For comparison purposes.
 {
 	/// <summary>
 	/// Sorting column.
