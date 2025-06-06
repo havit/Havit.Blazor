1ed45b36104699195e42947dd2d7eb02ee38662a
Xopabyteh <xopabyteh1337@gmail.com>
2025-06-06 15:08:37 +0200

    fix warning

--- Havit.Blazor.Components.Web.Bootstrap/Forms/HxValidationScrollableGroup.razor.cs
+++ Havit.Blazor.Components.Web.Bootstrap/Forms/HxValidationScrollableGroup.razor.cs
@@ -1,7 +1,7 @@
 namespace Havit.Blazor.Components.Web.Bootstrap;
 
 /// <summary>
-/// This component creates a grouping and keeps a contract for <see cref="HxScrollToValidationMessage"/>
+/// This component creates a grouping and keeps a contract for <see cref="HxScrollToValidationMessageContainer"/>
 /// so that it can scroll to the input group properly
 /// </summary>
 public partial class HxValidationScrollableGroup
